807ee1800323b16d980d2f8611d7b5b9c55a637e
Siyuan Feng <Hzfengsy@sjtu.edu.cn>
2019-11-21 16:19:46 -0800

    add TeLower pass and enable to run Te IR (#15)
    * add function data structure
    add TeLower pass to transform Te to current IR
    enable to run Te IR
    
    * address comments
    
    * unify terminology

--- python/tvm/driver/build_module.py
+++ python/tvm/driver/build_module.py
@@ -149,6 +149,8 @@ def lower(sch,
     # Phase 0
     if isinstance(sch, schedule.Schedule):
         stmt = form_body(sch)
+    elif isinstance(sch, container.TeFunction):
+        stmt = sch.body
 
     for f in lower_phase0:
         stmt = f(stmt)
@@ -157,7 +159,8 @@ def lower(sch,
     binds, arg_list = get_binds(args, compact, binds)
 
     # Phase 1
-    stmt = ir_pass.RewriteForTensorCore(stmt, sch, binds)
+    if isinstance(sch, schedule.Schedule):
+        stmt = ir_pass.RewriteForTensorCore(stmt, sch, binds)
     stmt = ir_pass.StorageFlatten(stmt, binds, 64, cfg.instrument_bound_checkers)
     stmt = ir_pass.CanonicalSimplify(stmt)
     for f in lower_phase1:
