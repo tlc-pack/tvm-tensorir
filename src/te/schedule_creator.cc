10d5a23872f57bc3d0fa53c3162f230628733d82
Siyuan Feng <Hzfengsy@sjtu.edu.cn>
2019-12-01 13:38:27 -0800

    [TE] introduce schedule, dependency graph and support fuse and split (#17)
    * fix lint
    
    * introduce dependency graph
    
    * enable create schedule
    
    * support get axes
    
    * fix lint
    
    * revert Set
    
    * add schedule primitive fuse
    
    * address comment
    
    * support split

---
+++ src/te/schedule_creator.cc
@@ -0,0 +1,94 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WAStmtStmtANTIES OStmt CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/*!
+ *  Copyright (c) by Contributors
+ * \file schedule_creator.cc
+ */
+
+#include <vector>
+#include "schedule_creator.h"
+
+namespace tvm {
+namespace te {
+
+ScheduleCreator::ScheduleCreator(Function func)
+    : func_(func) {}
+
+Schedule ScheduleCreator::Create() {
+  NodePtr<ScheduleNode> node = make_node<ScheduleNode>();
+  Stmt new_stmt = Mutate(func_->body);
+  Function new_func = Function(func_->params, func_->buffer_map, func_->name, new_stmt);
+  DependencyGraph dependency_graph(func_);
+  return Schedule(new_func, DependencyGraph(func_), write_map_);
+}
+
+Stmt ScheduleCreator::Mutate_(const BlockNode* op, const tvm::Stmt& s) {
+  Stmt body = this->Mutate(op->body);
+  Block new_block = te::Block(op->iter_vars,
+                              op->values,
+                              op->reads,
+                              op->writes,
+                              body,
+                              op->predicate,
+                              op->annotations,
+                              op->tag);
+
+  for (const auto& write : op->writes) {
+    Array<Block> array;
+    if (write_map_.count(write->buffer)) {
+      array = write_map_.at(write->buffer);
+    }
+    array.push_back(new_block);
+    write_map_.Set(write->buffer, array);
+  }
+  return new_block;
+}
+
+Stmt ScheduleCreator::Mutate_(const te::LoopNode* op, const Stmt& s) {
+  Stmt body = this->Mutate(op->body);
+  return te::Loop(op->loop_var, op->min, op->extent, op->annotations, body);
+}
+
+Stmt ScheduleCreator::Mutate_(const te::SeqStmtNode* op, const Stmt& s) {
+  std::vector<Stmt> new_stmt(op->size());
+  for (size_t i = 0; i < op->size(); ++i) {
+    Stmt old_elem = (*op)[i];
+    Stmt new_elem = Mutate(old_elem);
+    new_stmt[i] = new_elem;
+  }
+  return te::SeqStmt(new_stmt);
+}
+
+Stmt ScheduleCreator::Mutate_(const ir::Block* op, const Stmt& s) {
+  std::vector<Stmt> new_stmt;
+  do {
+    new_stmt.push_back(Mutate(op->first));
+    if (const ir::Block* t = op->rest.as<ir::Block>()) {
+      op = t;
+    } else {
+      new_stmt.push_back(Mutate(op->rest));
+      break;
+    }
+  } while (op);
+  return te::SeqStmt(new_stmt);
+}
+
+}  // namespace te
+}  // namespace tvm
--- src/te/util.cc
+++ src/te/util.cc
@@ -17,31 +17,14 @@
  * under the License.
  */
 
-/*!
- * \file tvm/include/te/transform.h
- * \brief Additional transform util for TE
- */
-#ifndef TVM_TE_TRANSFORM_H_
-#define TVM_TE_TRANSFORM_H_
-
-#include <tvm/expr.h>
-#include <tvm/buffer.h>
-#include <tvm/tensor.h>
-#include <tvm/te/ir.h>
-#include <tvm/ir_pass.h>
+#include "util.h"
+#include <tvm/ir_mutator.h>
 
 namespace tvm {
 namespace te {
-/*!
- * \brief Lower TE IR to current TVM IR. It is a temporary pass and will be
- * removed after rewriting all IR passes.
- *
- * \param func The TeFunc to be lowered
- * \param tensor_map Tensors to bind to the argument during lowering.
- * \return Transformed stmt.
- */
-Function TeLower(Function func, Map<Buffer, Tensor> tensor_map);
+
+
+
+
 }  // namespace te
 }  // namespace tvm
-
-#endif  // TVM_TE_TRANSFORM_H_
