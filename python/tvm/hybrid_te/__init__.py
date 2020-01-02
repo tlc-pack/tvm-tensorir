46449f75b545980eda426835908b96f4b08b4c75
spectrometerHBH <spectrometer@sjtu.edu.cn>
2020-01-02 12:51:59 +0800

    [Hybrid] refactor intrin, allow register external functions with argument type checking, add a testcase

--- python/tvm/hybrid_te/__init__.py
+++ python/tvm/hybrid_te/__init__.py
@@ -18,18 +18,51 @@
 
 from __future__ import absolute_import as _abs
 
+import inspect
+
 from . import intrin
 from .parser import source_to_op
 from .utils import _pruned_source
 from .._ffi.base import decorate
 
 
+def register(origin_func, scope_name="global", need_parser_node=False):
+    scope_dict = {"global": intrin.GlobalScope, "for": intrin.ForScope, "with": intrin.WithScope}
+    if scope_name not in scope_dict.keys():
+        raise RuntimeError("TVM Hybrid Script register error : scope should be \"global\", \"for\" or \"block\"")
+    else:
+        scope = scope_dict[scope_name]
+
+    full_arg_spec = inspect.getfullargspec(origin_func)
+    type_hints, args, defaults = full_arg_spec.annotations, full_arg_spec.args, full_arg_spec.defaults
+    if defaults is None:
+        defaults = tuple()
+    if need_parser_node:
+        args = args[2:]
+
+    if full_arg_spec.varargs is not None:
+        raise RuntimeError("TVM Hybrid Script register error : variable argument is not supported now")
+    if full_arg_spec.varkw is not None:
+        raise RuntimeError("TVM Hybrid Script register error : variable keyword argument is not supported now")
+    if not len(full_arg_spec.kwonlyargs) == 0:
+        raise RuntimeError("TVM Hybrid Script register error : keyword only argument is not supported now")
+
+    arg_list = list()
+    for arg in args[: len(args) - len(defaults)]:
+        arg_list.append((arg, type_hints.get(arg, None)))
+    for default, arg in zip(defaults, args[len(args) - len(defaults):]):
+        arg_list.append((arg, type_hints.get(arg, None), default))
+
+    intrin.register_func(scope, origin_func.__name__, origin_func, arg_list, need_parser_and_node=need_parser_node,
+                         need_return="return" in type_hints.keys())
+
+
 def init_scope():
-    intrin.register_buffer_bind(intrin.GlobalScope)
-    intrin.register_buffer_allocate(intrin.GlobalScope)
-    intrin.register_block_vars(intrin.GlobalScope)
-    intrin.register_block(intrin.WithScope)
-    intrin.register_range(intrin.ForScope)
+    register(intrin.buffer_bind, need_parser_node=True)
+    register(intrin.buffer_allocate, need_parser_node=True)
+    register(intrin.block_vars, need_parser_node=True)
+    register(intrin.block, scope_name="with", need_parser_node=True)
+    register(intrin.range, scope_name="for", need_parser_node=True)
 
 
 def script(origin_func):
---
+++ tests/python/te/test_hybrid_te_register.py
@@ -0,0 +1,83 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+import numpy as np
+import tvm
+
+
+def add(a: tvm.expr.Expr, b: tvm.expr.Expr) -> tvm.expr.Expr:
+    return a + b
+
+
+def mul(a: tvm.expr.Expr, b: tvm.expr.Expr) -> tvm.expr.Expr:
+    return a * b
+
+
+@tvm.hybrid_te.script
+def element_wise(a, c):
+    A = buffer_bind(a, (16, 16), "float32", name="A")
+    C = buffer_bind(c, (16, 16), "float32", name="C")
+
+    with block([], [], A[0: 16, 0: 16], C[0: 16, 0: 16], name="root"):
+        B = buffer_allocate((16, 16), "float32", name="B")
+
+        for i in range(0, 16):
+            for j in range(0, 16):
+                with block([vi(0, 16), vj(0, 16)], [i, j], A[vi: vi + 1, vj: vj + 1], B[vi: vi + 1, vj: vj + 1],
+                           name="B"):
+                    B[vi, vj] = mul(A[vi, vj], 2)
+
+        for i in range(0, 16):
+            for j in range(0, 16):
+                with block([vi(0, 16), vj(0, 16)], [i, j], B[vi: vi + 1, vj: vj + 1], C[vi: vi + 1, vj: vj + 1],
+                           name="C"):
+                    C[vi, vj] = add(B[vi, vj], 1)
+
+
+def test_element_wise(a, c):
+    m, n = 16, 16
+    func, tensors, tensor_map = element_wise(a, c)
+
+    assert isinstance(func.body, tvm.stmt.TeBlock)
+    assert isinstance(func.body.body, tvm.stmt.Block)
+    assert isinstance(func.body.body.first, tvm.stmt.Loop)
+    assert isinstance(func.body.body.first.body, tvm.stmt.Loop)
+    assert isinstance(func.body.body.first.body.body, tvm.stmt.TeBlock)
+
+    assert isinstance(func.body.body.rest, tvm.stmt.Loop)
+    assert isinstance(func.body.body.rest.body, tvm.stmt.Loop)
+    assert isinstance(func.body.body.rest.body.body, tvm.stmt.TeBlock)
+
+    func = tvm.ir_pass.TeLower(func, tensor_map)
+    print(func)
+    lower_func = tvm.lower(func, tensors)
+
+    func = tvm.build(lower_func)
+    a_np = np.random.uniform(size=(m, n)).astype("float32")
+    a = tvm.nd.array(a_np)
+    c = tvm.nd.array(np.zeros((m, n)).astype("float32"))
+    func(a, c)
+    tvm.testing.assert_allclose(c.asnumpy(), a_np * 2 + 1, rtol=1e-6)
+
+
+if __name__ == '__main__':
+    tvm.hybrid_te.register(add)
+    tvm.hybrid_te.register(mul)
+    a = tvm.var("a")
+    c = tvm.var("c")
+
+    test_element_wise(a, c)
