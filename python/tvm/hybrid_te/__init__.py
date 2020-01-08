40b304e89df9f8716f9552d1f186dc5b6c222bbf
Bohan Hou <32121147+spectrometerHBH@users.noreply.github.com>
2020-01-08 10:05:58 +0800

    [Hybrid] python syntax parser (#20)
    * [Hybrid] python syntax parser
    
    * [Hybrid] add a testcase
    
    * [Hybrid] improve comments and fix bugs
    
    * [Hybrid] improve comments, refactor __internal_assert, add new testcases
    
    * [Hybrid] improve error report message, refactor intrin
    
    * [Hybrid] separate ScopeEmitter from parser
    
    * [Hybrid] refactor type check
    
    * [Hybrid] refactor intrin
    
    * [Hybrid] refactor intrin, allow register external functions with argument type checking, add a testcase
    
    * [Hybrid] address comments, fix a bug in te/ir.h
    
    * [Hybrid] remove type check
    
    * [Hybrid] python syntax parser
    
    * [Hybrid] add a testcase
    
    * [Hybrid] improve comments and fix bugs
    
    * [Hybrid] improve comments, refactor __internal_assert, add new testcases
    
    * [Hybrid] improve error report message, refactor intrin
    
    * [Hybrid] separate ScopeEmitter from parser
    
    * [Hybrid] refactor type check
    
    * [Hybrid] refactor intrin
    
    * [Hybrid] refactor intrin, allow register external functions with argument type checking, add a testcase
    
    * [Hybrid] address comments, fix a bug in te/ir.h
    
    * [Hybrid] remove type check
    
    * [Hybrid] refactor intrin, scope_handler, special_stmt
    
    * [Hybrid] address comments
    
    * [Hybrid] clean code, improve error reporting & testcase
    
    * [Hybrid] clean code
    
    * [Hybrid] clean code

---
+++ python/tvm/hybrid_te/__init__.py
@@ -0,0 +1,66 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+"""Hybrid Programming APIs of TVM Python Package, aimed to support TE IR"""
+
+from __future__ import absolute_import as _abs
+
+from . import utils, registry, intrin, special_stmt, scope_handler
+from .parser import source_to_op
+from .utils import _pruned_source
+from .._ffi.base import decorate
+
+
+def register(origin_func):
+    """Register an external function to parser under intrin
+
+    The registered function ought to have return value.
+
+    Parameters
+    ----------
+    origin_func : python function
+        The function to be registered.
+        Default value in parameter list is supported.
+    """
+    registry.register_intrin(origin_func)
+
+
+def _init_scope():
+    """Register primitive functions"""
+    registry.register_special_stmt(special_stmt.buffer_bind)
+    registry.register_special_stmt(special_stmt.buffer_allocate)
+    registry.register_special_stmt(special_stmt.block_vars)
+    registry.register_scope_handler(scope_handler.block, scope_name="with_scope")
+    registry.register_scope_handler(scope_handler.range, scope_name="for_scope")
+
+
+def script(origin_func):
+    """Decorate a python function function as hybrid script.
+
+    The hybrid function support parsing to the internal TE IR.
+
+    Returns
+    -------
+    function : TeFunction
+        The TeFunction in IR.
+    """
+
+    def wrapped_func(func, *args, **kwargs):
+        _init_scope()
+        src = _pruned_source(func)
+        return source_to_op(func.__code__.co_firstlineno, src, *args, **kwargs)
+
+    return decorate(origin_func, wrapped_func)
