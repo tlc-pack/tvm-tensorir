06643dad570da4df2a13ef01bfe5303fd3b8f73d
Bohan Hou <32121147+spectrometerHBH@users.noreply.github.com>
2020-01-08 10:05:58 +0800

    [Hybrid] python syntax parser (#20)
    * [Hybrid] python syntax parser
    
    * [Hybrid] add a testcase
    
    * [Hybrid] improve comments and fix bugs
    
    * [Hybrid] improve comments, refactor __internal_assert, add new testcases
    
    * [Hybrid] improve error report message, refactor intrin
    
    * [Hybrid] separate ScopeEmitter from parser
    
    * [Hybrid] refactor type check
    
    * [Hybrid] refactor intrin
    
    * [Hybrid] refactor intrin, allow register external functions with argument type checking, add a testcase
    
    * [Hybrid] address comments, fix a bug in te/ir.h
    
    * [Hybrid] remove type check
    
    * [Hybrid] python syntax parser
    
    * [Hybrid] add a testcase
    
    * [Hybrid] improve comments and fix bugs
    
    * [Hybrid] improve comments, refactor __internal_assert, add new testcases
    
    * [Hybrid] improve error report message, refactor intrin
    
    * [Hybrid] separate ScopeEmitter from parser
    
    * [Hybrid] refactor type check
    
    * [Hybrid] refactor intrin
    
    * [Hybrid] refactor intrin, allow register external functions with argument type checking, add a testcase
    
    * [Hybrid] address comments, fix a bug in te/ir.h
    
    * [Hybrid] remove type check
    
    * [Hybrid] refactor intrin, scope_handler, special_stmt
    
    * [Hybrid] address comments
    
    * [Hybrid] clean code, improve error reporting & testcase
    
    * [Hybrid] clean code
    
    * [Hybrid] clean code

---
+++ python/tvm/hybrid_te/scope_handler.py
@@ -0,0 +1,69 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+"""Hybrid Script Parser Scope Handler Functions
+
+This module provides the functions registered into parser under with_scope or for_scope category.
+Scope handler functions are used to handle such scenarios.
+
+.. code-block:: python
+
+    for x in name():
+    with name():
+
+Typically, a scope handler function has no return value and accepts parser and node as its first 2 arguments.
+"""
+
+from .. import api as _api
+from .. import ir_pass as _pass
+from .. import make as _make
+
+
+def block(parser, node, block_vars, values, reads, writes, predicate=True, annotations=[], name=""):
+    """ With scope handler function block(block_vars, values, reads, writes, predicate, annotations, name)
+
+    Example
+    -------
+    .. code-block:: python
+
+        with block([vi(0, 128), vj(0, 128)], [i, j], reads=[], writes=C[vi : vi + 1, vj : vj + 1], name="init"):
+
+    """
+    if not isinstance(reads, list):
+        reads = [reads]
+    if not isinstance(writes, list):
+        writes = [writes]
+    parser.scope_emitter.new_scope(is_block=True)
+    for stmt in node.body:
+        parser.visit(stmt)
+    for block_var in block_vars:
+        parser.remove_symbol(block_var.var.name)
+    parser.scope_emitter.emit(
+        _make.TeBlock(block_vars, values, reads, writes, parser.scope_emitter.pop_seq(), predicate,
+                      parser.scope_emitter.allocate_stack.pop(), annotations, name))
+
+
+def range(parser, node, begin, end):
+    """ For scope handler function range(begin, end)"""
+    extent = end if begin == 0 else _pass.Simplify(end - begin)
+    loop_var_name = node.target.id
+    loop_var = _api.var(loop_var_name, dtype="int32")
+    parser.update_symbol(loop_var_name, parser.Symbol.LoopVar, loop_var)
+    parser.scope_emitter.new_scope()
+    for stmt in node.body:
+        parser.visit(stmt)
+    parser.scope_emitter.emit(_make.Loop(loop_var, begin, extent, [], parser.scope_emitter.pop_seq()))
+    parser.remove_symbol(loop_var_name)
