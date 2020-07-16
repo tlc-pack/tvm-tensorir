bb8912f496735f2a61b4bc7e3a4f44474d3cf385
Junru Shao <junrushao1994@gmail.com>
2020-07-16 15:53:35 -0700

    [Refactor] Schedule Creation; Schedule Validation (#71)
    * Re-implement: schedule creation, loop validation; Refactor: move creation code to schedule_create.cc, move validation code to schedule_validate.cc
    
    * Update
    
    * Add StmtSRef::GetStmt<SomeNode>() in preparation of removing DowncastPtr

--- src/tir/schedule/compute_at.cc
+++ src/tir/schedule/compute_at.cc
@@ -162,7 +162,7 @@ Stmt RegenerateLoops(const StmtSRef& block_sref, const StmtSRef& parent_loop_sre
       body = loop;
     }
   }
-  Loop loop = Downcast<Loop>(GetRef<Stmt>(parent_loop_sref->node));
+  Loop loop = Downcast<Loop>(GetRef<Stmt>(parent_loop_sref->stmt));
   Array<Stmt> stmts = GetChildren(loop);
   stmts.insert(stmts.begin() + insert_pos, body);
 
@@ -220,66 +220,6 @@ std::vector<Range> GatherRequirements(const Array<TensorRegion>& produce_regions
   return ret;
 }
 
-// region cover check
-bool ScheduleNode::CheckRegionCover(const StmtSRef& consumer) const {
-  if (consumer->parent == nullptr) return true;
-  const auto* block = DowncastPtr<BlockNode>(consumer->node);
-  StmtSRef scope_sref = GetScope(consumer);
-  const Scope& scope = scopes_.at(scope_sref);
-
-  // Gather all the producers
-  std::unordered_map<const VarNode*, std::vector<StmtSRef>> producers;
-  std::unordered_map<const VarNode*, std::vector<const TensorRegionNode*>> produce_regions;
-  const auto& successors = scope.GetSuccessors(consumer);
-
-  for (const auto& edge : successors) {
-    if (edge->type == DepType::kRAW) {
-      const auto* producer_block = DowncastPtr<BlockNode>(edge->dst->node);
-      for (const auto& output_region : producer_block->writes) {
-        const auto* bufferVar = output_region->buffer->data.operator->();
-        producers[bufferVar].push_back(edge->dst);
-        produce_regions[bufferVar].push_back(output_region.operator->());
-      }
-    }
-  }
-
-  for (const auto& input_region : block->reads) {
-    const auto* bufferVar = input_region->buffer->data.operator->();
-    std::vector<StmtSRef>& nodes = producers[bufferVar];
-    if (nodes.empty()) continue;
-    std::vector<const TensorRegionNode*> regions = produce_regions[bufferVar];
-    // calculate the LCA
-    nodes.push_back(consumer);
-    const StmtSRef& lca = LowestCommonAncestor(nodes, scope_sref);
-    nodes.pop_back();
-    // prepare check function
-    auto check_cover = [](const TensorRegion& read, const TensorRegion& write) -> bool {
-      CHECK_EQ(read->region.size(), write->region.size());
-      for (size_t i = 0; i < read->region.size(); ++i) {
-        auto read_min = read->region[i]->min;
-        auto write_min = write->region[i]->min;
-        auto read_max = read_min + read->region[i]->extent;
-        auto write_max = write_min + write->region[i]->extent;
-        arith::Analyzer analyzer;
-        if (!analyzer.CanProve(read_min >= write_min)
-            || !analyzer.CanProve(read_max <= write_max)) {
-          LOG(WARNING) << "Cannot prove the region cover: producer " << read << " consumer "
-                       << write;
-          return false;
-        }
-      }
-      return true;
-    };
-    TensorRegion read = RelaxRegion(consumer, lca, input_region);
-    for (size_t i = 0; i < nodes.size(); ++i) {
-      TensorRegion write = RelaxRegion(nodes[i], lca, GetRef<TensorRegion>(regions[i]));
-      if (!check_cover) return false;
-    }
-  }
-
-  return true;
-}
-
 class StmtReplacer : public StmtMutator {
  public:
   explicit StmtReplacer(const std::unordered_map<Stmt, Stmt, ObjectHash, ObjectEqual>& repalce_map)
@@ -321,17 +261,17 @@ void ScheduleNode::compute_at(const StmtSRef& block_sref, const StmtSRef& loop_s
    */
 
   // Check
-  const auto* block = DowncastPtr<BlockNode>(block_sref->node);
-  const auto* loop = DowncastPtr<LoopNode>(loop_sref->node);
+  const auto* block = DowncastPtr<BlockNode>(block_sref->stmt);
+  const auto* loop = DowncastPtr<LoopNode>(loop_sref->stmt);
   CHECK(block != nullptr) << block_sref << "is not a block sref";
   CHECK(loop != nullptr) << loop_sref << "is not a loop sref";
 
   // Check the block and the loop are at the same scope
-  CHECK_EQ(GetScope(block_sref), GetScope(loop_sref))
+  CHECK_EQ(GetParentScope(block_sref), GetParentScope(loop_sref))
     << "Cannot compute_at between different scope";
-  const StmtSRef& scope_sref = GetScope(block_sref);
-  const Scope& scope = scopes_.at(scope_sref);
-  const auto* scope_block = DowncastPtr<BlockNode>(scope_sref->node);
+  const StmtSRef& scope_sref = GetParentScope(block_sref);
+  const Scope& scope = scopes.at(scope_sref);
+  const auto* scope_block = DowncastPtr<BlockNode>(scope_sref->stmt);
 
   // Check complete block
   CHECK(scope.IsComplete(block_sref) || scope.IsReduction(block_sref))
@@ -341,7 +281,7 @@ void ScheduleNode::compute_at(const StmtSRef& block_sref, const StmtSRef& loop_s
   StmtSRef sub_tree_root = block_sref;
   while (sub_tree_root.defined()) {
     auto node = GetRef<StmtSRef>(sub_tree_root->parent);
-    if (GetRef<Stmt>(node->node).as<BlockNode>()) {
+    if (GetRef<Stmt>(node->stmt).as<BlockNode>()) {
       break;
     } else {
       sub_tree_root = node;
@@ -412,14 +352,14 @@ void ScheduleNode::compute_at(const StmtSRef& block_sref, const StmtSRef& loop_s
 
   StmtSRef lca = LowestCommonAncestor({block_sref, loop_sref}, root);
   std::unordered_map<Stmt, Stmt, ObjectHash, ObjectEqual> replace_map;
-  replace_map[GetRef<Stmt>(loop_sref->node)] = new_stmt;
+  replace_map[GetRef<Stmt>(loop_sref->stmt)] = new_stmt;
   replace_map[removed.first] = removed.second;
 
   // Mutate the AST with Replace
-  Stmt replaced_stmt = StmtReplacer(replace_map)(GetRef<Stmt>(lca->node));
+  Stmt replaced_stmt = StmtReplacer(replace_map)(GetRef<Stmt>(lca->stmt));
   if (replaced_stmt.as<BlockNode>()) {
     Map<Block, Block> block_map;
-    auto block_scope = Downcast<Block>(GetRef<Stmt>(scope_sref->node));
+    auto block_scope = Downcast<Block>(GetRef<Stmt>(scope_sref->stmt));
     block_map.Set(Downcast<Block>(replaced_stmt), block_scope);
     this->Replace(lca, replaced_stmt, block_map);
   } else {
