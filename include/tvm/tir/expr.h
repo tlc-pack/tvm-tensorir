5c7d9a8057b762672230088de2e2f7f5388055aa
Junru Shao <junrushao1994@gmail.com>
2020-07-23 16:21:04 -0700

    [Refactor] Refactor tir::Scope (#74)

--- include/tvm/tir/expr.h
+++ include/tvm/tir/expr.h
@@ -609,7 +609,7 @@ class BufferLoadNode : public PrimExprNode {
   Array<PrimExpr> indices;
 
   void VisitAttrs(AttrVisitor* v) {
-    v->Visit("dtype", &(this->dtype));
+    v->Visit("dtype", &dtype);
     v->Visit("buffer", &buffer);
     v->Visit("indices", &indices);
     v->Visit("span", &span);
