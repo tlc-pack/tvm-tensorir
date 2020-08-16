fc69d08f7b51f583e5ffb36cd4ecc7afb7c139f3
Bohan Hou <32121147+spectrometerHBH@users.noreply.github.com>
2020-08-16 12:55:21 +0800

    [TIR][HYBRID] refactor (#92)

--- python/tvm/hybrid/intrin.py
+++ python/tvm/hybrid/intrin.py
@@ -15,130 +15,124 @@
 # specific language governing permissions and limitations
 # under the License.
 """Hybrid Script Parser Intrinsic Functions
-
-This module provides the functions registered into parser under intrin category.
-Typically, a intrin function has return value.
-Meanwhile, user can register intrin functions into parser.
-
-Example
--------
-
-.. code-block:: python
-
-    def add(a, b):
-        return a + b
-    def mul(a, b=1):
-        return a * b
-
+IRNodes (StmtNodes without body, PrimExprNodes and more) are called intrins
 """
-
+# pylint: disable=redefined-builtin
 import tvm.tir
 from .registry import register_intrin
 
 
-@register_intrin
+@register_intrin()
 def bool(imm):
-    return tvm.tir.const(imm.value, "bool")
+    return tvm.tir.const(imm, "bool")
 
 
-@register_intrin
+@register_intrin()
+def int8(imm):
+    return tvm.tir.const(imm, "int8")
+
+
+@register_intrin()
 def int16(imm):
-    return tvm.tir.const(imm.value, "int16")
+    return tvm.tir.const(imm, "int16")
 
 
-@register_intrin
+@register_intrin()
 def int32(imm):
-    return tvm.tir.const(imm.value, "int32")
+    return tvm.tir.const(imm, "int32")
 
 
-@register_intrin
+@register_intrin()
 def int64(imm):
-    return tvm.tir.const(imm.value, "int64")
+    return tvm.tir.const(imm, "int64")
 
 
-@register_intrin
+@register_intrin()
 def uint8(imm):
-    return tvm.tir.const(imm.value, "uint8")
+    return tvm.tir.const(imm, "uint8")
 
 
-@register_intrin
+@register_intrin()
 def uint16(imm):
-    return tvm.tir.const(imm.value, "uint16")
+    return tvm.tir.const(imm, "uint16")
 
 
-@register_intrin
+@register_intrin()
 def uint32(imm):
-    return tvm.tir.const(imm.value, "uint32")
+    return tvm.tir.const(imm, "uint32")
 
 
-@register_intrin
+@register_intrin()
 def uint64(imm):
-    return tvm.tir.const(imm.value, "uint64")
+    return tvm.tir.const(imm, "uint64")
 
 
-@register_intrin
+@register_intrin()
+def float8(imm):
+    return tvm.tir.const(imm, "float8")
+
+
+@register_intrin()
 def float16(imm):
-    return tvm.tir.const(imm.value, "float16")
+    return tvm.tir.const(imm, "float16")
 
 
-@register_intrin
+@register_intrin()
 def float32(imm):
-    return tvm.tir.const(imm.value, "float32")
+    return tvm.tir.const(imm, "float32")
 
 
-@register_intrin
+@register_intrin()
 def float64(imm):
-    return tvm.tir.const(imm.value, "float64")
+    return tvm.tir.const(imm, "float64")
 
 
-@register_intrin
+@register_intrin()
 def floordiv(x, y):
     return tvm.tir.floordiv(x, y)
 
 
-@register_intrin
+@register_intrin()
 def floormod(x, y):
     return tvm.tir.floormod(x, y)
 
 
-@register_intrin
+@register_intrin()
 def load(dtype, var, index, predicate=True):
     return tvm.tir.Load(dtype, var, index, predicate)
 
 
-@register_intrin
+@register_intrin()
 def cast(dtype, value):
     return tvm.tir.Cast(dtype, value)
 
 
-@register_intrin
+@register_intrin()
 def ramp(base, stride, lanes):
-    lanes = lanes.value if not isinstance(lanes, int) else lanes
     return tvm.tir.Ramp(base, stride, lanes)
 
 
-@register_intrin
+@register_intrin()
 def broadcast(value, lanes):
-    lanes = lanes.value if not isinstance(lanes, int) else lanes
     return tvm.tir.Broadcast(value, lanes)
 
 
-@register_intrin
+@register_intrin()
 def evaluate(value):
     return tvm.tir.Evaluate(value)
 
 
-@register_intrin
+@register_intrin()
 def store(var, index, value, predicate=True):
     return tvm.tir.Store(var, value, index, predicate)
 
 
-@register_intrin
+@register_intrin()
 def iter_var(var, dom, iter_type, thread_tag):
     iter_type = getattr(tvm.tir.IterVar, iter_type)
     return tvm.tir.IterVar(dom, var, iter_type, thread_tag)
 
 
-@register_intrin
+@register_intrin()
 def max(a, b):  # pylint: disable=redefined-builtin
     return tvm.tir.Max(a, b)
