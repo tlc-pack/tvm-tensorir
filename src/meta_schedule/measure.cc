ba22574cc748a882f60bca4db97cd2b9d7c1ebca
Junru Shao <junrushao1994@gmail.com>
2020-09-16 14:45:10 -0700

    add measure c++ src

---
+++ src/meta_schedule/measure.cc
@@ -0,0 +1,232 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+#include "./measure.h"  // NOLINT(build/include)
+
+#include <tvm/node/node.h>
+
+#include <algorithm>
+
+namespace tvm {
+namespace meta_schedule {
+
+TVM_REGISTER_NODE_TYPE(MeasureInputNode);
+TVM_REGISTER_NODE_TYPE(BuildResultNode);
+TVM_REGISTER_NODE_TYPE(MeasureResultNode);
+TVM_REGISTER_OBJECT_TYPE(ProgramRunnerNode);
+TVM_REGISTER_OBJECT_TYPE(ProgramBuilderNode);
+TVM_REGISTER_OBJECT_TYPE(LocalBuilderNode);
+TVM_REGISTER_OBJECT_TYPE(RPCRunnerNode);
+
+static const char* ErrorNoToStr[] = {
+    "NoError",
+    "InstantiationError",
+    "CompileHostError",
+    "CompileDeviceError",
+    "RuntimeDeviceError",
+    "WrongAnswerError",
+    "BuildTimeoutError",
+    "RunTimeoutError",
+    "UnknownError",
+};
+
+/********** Constructors **********/
+
+MeasureInput::MeasureInput(Schedule sch) {
+  ObjectPtr<MeasureInputNode> n = make_object<MeasureInputNode>();
+  n->sch = std::move(sch);
+  data_ = std::move(n);
+}
+
+BuildResult::BuildResult(String filename, Array<te::Tensor> args, int error_no, String error_msg,
+                         double time_cost) {
+  ObjectPtr<BuildResultNode> n = make_object<BuildResultNode>();
+  n->filename = std::move(filename);
+  n->args = std::move(args);
+  n->error_no = error_no;
+  n->error_msg = std::move(error_msg);
+  n->time_cost = time_cost;
+  data_ = std::move(n);
+}
+
+MeasureResult::MeasureResult(Array<PrimExpr> costs, int error_no, String error_msg, double all_cost,
+                             double timestamp) {
+  ObjectPtr<MeasureResultNode> n = make_object<MeasureResultNode>();
+  n->costs = std::move(costs);
+  n->error_no = error_no;
+  n->error_msg = std::move(error_msg);
+  n->all_cost = all_cost;
+  n->timestamp = timestamp;
+  data_ = std::move(n);
+}
+
+LocalBuilder::LocalBuilder(int timeout, int n_parallel, const String& build_func) {
+  ObjectPtr<LocalBuilderNode> n = make_object<LocalBuilderNode>();
+  n->timeout = timeout;
+  n->n_parallel = n_parallel;
+  n->build_func = build_func;
+  data_ = std::move(n);
+}
+
+RPCRunner::RPCRunner(const String& key, const String& host, int port, int priority, int n_parallel,
+                     int timeout, int number, int repeat, int min_repeat_ms,
+                     double cooldown_interval, bool enable_cpu_cache_flush) {
+  ObjectPtr<RPCRunnerNode> n = make_object<RPCRunnerNode>();
+  n->key = key;
+  n->host = host;
+  n->port = port;
+  n->priority = priority;
+  n->timeout = timeout;
+  n->n_parallel = n_parallel;
+  n->number = number;
+  n->repeat = repeat;
+  n->min_repeat_ms = min_repeat_ms;
+  n->cooldown_interval = cooldown_interval;
+  n->enable_cpu_cache_flush = enable_cpu_cache_flush;
+  data_ = std::move(n);
+}
+
+/********** Shallow copy functions **********/
+
+MeasureInput MeasureInputNode::copy() const {
+  ObjectPtr<MeasureInputNode> n = make_object<MeasureInputNode>();
+  n->sch = sch;
+  return MeasureInput(n);
+}
+
+MeasureResult MeasureResultNode::copy() const {
+  ObjectPtr<MeasureResultNode> n = make_object<MeasureResultNode>();
+  n->costs = costs;
+  n->error_no = error_no;
+  n->error_msg = error_msg;
+  n->all_cost = all_cost;
+  n->timestamp = timestamp;
+  return MeasureResult(n);
+}
+
+/********** LocalBuilder **********/
+
+Array<BuildResult> LocalBuilderNode::Build(const Array<MeasureInput>& inputs, int verbose) {
+  if (const auto* f = runtime::Registry::Get("meta_schedule.local_builder.build")) {
+    Array<BuildResult> results = (*f)(inputs, timeout, n_parallel, build_func, verbose);
+    return results;
+  }
+  LOG(FATAL) << "meta_schedule.local_builder.build is not registered. "
+             << "This is a function registered in Python, "
+             << "make sure the TVM Python runtime has been loaded successfully.";
+  throw;
+}
+
+/********** RPCRunner **********/
+
+Array<MeasureResult> RPCRunnerNode::Run(const Array<MeasureInput>& inputs,
+                                        const Array<BuildResult>& build_results, int verbose) {
+  if (const auto* f = runtime::Registry::Get("meta_schedule.rpc_runner.run")) {
+    Array<MeasureResult> results =
+        (*f)(inputs, build_results, key, host, port, priority, n_parallel, timeout, number, repeat,
+             min_repeat_ms, cooldown_interval, enable_cpu_cache_flush, verbose);
+    return results;
+  } else {
+    LOG(FATAL) << "meta_schedule.rpc_runner.run is not registered. "
+               << "This is a function registered in Python, "
+               << "make sure the TVM Python runtime has been loaded successfully.";
+  }
+  return Array<MeasureResult>();
+}
+
+/********** Printing functions **********/
+
+TVM_STATIC_IR_FUNCTOR(ReprPrinter, vtable)
+    .set_dispatch<MeasureResultNode>([](const ObjectRef& ref, ReprPrinter* p) {
+      auto* node = static_cast<const MeasureResultNode*>(ref.get());
+      if (node->error_no == static_cast<int>(MeasureErrorNO::kNoError)) {
+        p->stream << "MeasureResult(cost:[";
+        auto old_config = p->stream.precision(4);
+        for (size_t i = 0; i < node->costs.size(); ++i) {
+          auto pf = node->costs[i].as<FloatImmNode>();
+          CHECK(pf != nullptr);
+          p->stream << pf->value;
+          if (i != node->costs.size() - 1) {
+            p->stream << ",";
+          }
+        }
+        p->stream.precision(old_config);
+        p->stream << "], ";
+        p->stream << "error_no:" << 0 << ", "
+                  << "all_cost:" << node->all_cost << ", "
+                  << "Tstamp:" << node->timestamp << ")";
+      } else {
+        p->stream << "MeasureResult("
+                  << "error_type:" << ErrorNoToStr[node->error_no] << ", "
+                  << "error_msg:" << node->error_msg << ", "
+                  << "all_cost:" << node->all_cost << ", "
+                  << "Tstamp:" << node->timestamp << ")";
+      }
+    });
+
+TVM_STATIC_IR_FUNCTOR(ReprPrinter, vtable)
+    .set_dispatch<BuildResultNode>([](const ObjectRef& ref, ReprPrinter* p) {
+      auto* node = static_cast<const BuildResultNode*>(ref.get());
+      p->stream << "BuildResult(" << node->filename << ", " << node->error_no << ", "
+                << node->time_cost << ")";
+    });
+
+/********** FFI **********/
+
+struct Internal {
+  /********** Constructors **********/
+  static MeasureInput CreateMeasureInput(Schedule sch) { return MeasureInput(sch); }
+  static BuildResult CreateBuildResult(String filename, Array<te::Tensor> args, int error_no,
+                                       String error_msg, double time_cost) {
+    return BuildResult(filename, args, error_no, error_msg, time_cost);
+  }
+  static MeasureResult CreateMeasureResult(Array<PrimExpr> costs, int error_no, String error_msg,
+                                           double all_cost, double timestamp) {
+    return MeasureResult(costs, error_no, error_msg, all_cost, timestamp);
+  }
+  static LocalBuilder CreateLocalBuilder(int timeout, int n_parallel, String build_func) {
+    return LocalBuilder(timeout, n_parallel, build_func);
+  }
+  static RPCRunner CreateRPCRunner(String key, String host, int port, int priority, int n_parallel,
+                                   int timeout, int number, int repeat, int min_repeat_ms,
+                                   double cooldown_interval, bool enable_cpu_cache_flush) {
+    return RPCRunner(key, host, port, priority, n_parallel, timeout, number, repeat, min_repeat_ms,
+                     cooldown_interval, enable_cpu_cache_flush);
+  }
+  /********** Member methods **********/
+  static Array<BuildResult> ProgramBuilderBuild(ProgramBuilder builder, Array<MeasureInput> inputs,
+                                                int verbose) {
+    return builder->Build(inputs, verbose);
+  }
+  static Array<MeasureResult> ProgramRunnerRun(ProgramRunner runner, Array<MeasureInput> inputs,
+                                               Array<BuildResult> build_results, int verbose) {
+    return runner->Run(inputs, build_results, verbose);
+  }
+};
+
+TVM_REGISTER_GLOBAL("meta_schedule.MeasureInput").set_body_typed(Internal::CreateMeasureInput);
+TVM_REGISTER_GLOBAL("meta_schedule.BuildResult").set_body_typed(Internal::CreateBuildResult);
+TVM_REGISTER_GLOBAL("meta_schedule.MeasureResult").set_body_typed(Internal::CreateMeasureResult);
+TVM_REGISTER_GLOBAL("meta_schedule.LocalBuilder").set_body_typed(Internal::CreateLocalBuilder);
+TVM_REGISTER_GLOBAL("meta_schedule.RPCRunner").set_body_typed(Internal::CreateRPCRunner);
+TVM_REGISTER_GLOBAL("meta_schedule.ProgramBuilderBuild")
+    .set_body_typed(Internal::ProgramBuilderBuild);
+TVM_REGISTER_GLOBAL("meta_schedule.ProgramRunnerRun").set_body_typed(Internal::ProgramRunnerRun);
+
+}  // namespace meta_schedule
+}  // namespace tvm
---
+++ src/meta_schedule/measure.h
@@ -0,0 +1,374 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/*!
+ * \brief Distributed measurement infrastructure to measure the runtime costs of tensor programs.
+ * These functions are responsible for building the tvm module, uploading it to remote devices,
+ * recording the running time costs, and checking the correctness of the output.
+ *
+ * The measurement is separated into two steps: build and run.
+ * A builder builds the executable binary files and a runner runs the binary files to get the
+ * measurement results. The flow of data structures is
+ *
+ *                 `ProgramBuilder`                 `ProgramRunner`
+ * `MeasureInput` -----------------> `BuildResult` ----------------> `MeasureResult`
+ *
+ *
+ * Taken and modified from Ansor
+ * The core functions is implemented in python to utilize python's multiprocessing
+ * and error handling (see also `python/tvm/meta_schedule/measure.py`).
+ * This c++ file is just a wrapper for the python functions.
+ */
+
+#ifndef SRC_META_SCHEDULE_MEASURE_H_
+#define SRC_META_SCHEDULE_MEASURE_H_
+
+#include <string>
+#include <unordered_map>
+#include <utility>
+
+#include "./schedule.h"
+
+namespace tvm {
+namespace meta_schedule {
+
+/*! \brief The error code of one measurement */
+enum class MeasureErrorNO : int {
+  /*! \brief No error. */
+  kNoError = 0,
+  /*! \brief Errors happen when apply transform steps from init state. */
+  kInstantiationError = 1,
+  /*! \brief Errors happen when compiling code on host. (when build module) */
+  kCompileHostError = 2,
+  /*! \brief Errors happen when compiling code on device. (when load module) */
+  kCompileDeviceError = 3,
+  /*! \brief Errors happen when run program on device. */
+  kRuntimeDeviceError = 4,
+  /*! \brief Answer is wrong when compared to a reference output. */
+  kWrongAnswerError = 5,
+  /*! \brief Timeout during compilation. */
+  kBuildTimeoutError = 6,
+  /*! \brief Timeout during run. */
+  kRunTimeoutError = 7,
+  /*! \brief Unknown error. */
+  kUnknownError = 8,
+};
+
+/********** MeasureInput **********/
+
+// Inputs and results of one measurement
+
+class MeasureInput;
+
+/*! \brief Store the input of a measurement */
+class MeasureInputNode : public Object {
+ public:
+  /*! \brief The search task. */
+  Schedule sch{nullptr};
+
+  void VisitAttrs(tvm::AttrVisitor* v) { v->Visit("sch", &sch); }
+
+  /*! \brief Do shallow copy. */
+  MeasureInput copy() const;
+
+  static constexpr const char* _type_key = "meta_schedule.MeasureInput";
+  TVM_DECLARE_FINAL_OBJECT_INFO(MeasureInputNode, Object);
+};
+
+/*!
+ * \brief Managed reference to MeasureInputNode.
+ * \sa MeasureInputNode
+ */
+class MeasureInput : public ObjectRef {
+ public:
+  /*!
+   * \brief The constructor.
+   * \param task The SearchTask of this measure.
+   * \param state The State to be measured.
+   */
+  explicit MeasureInput(Schedule sch);
+
+  TVM_DEFINE_OBJECT_REF_METHODS(MeasureInput, ObjectRef, MeasureInputNode);
+};
+
+/********** BuildResult **********/
+
+/*! \brief Store the result of a build. */
+class BuildResultNode : public Object {
+ public:
+  /*! \brief The filename of built binary file. */
+  String filename;
+  /*! \brief The arguments. */
+  Array<te::Tensor> args;
+  /*! \brief The error code. (0 means no error, see MeasureErrorNO) */
+  int error_no;
+  /*! \brief The error message if there is any error. */
+  String error_msg;
+  /*! \brief The time cost of build. */
+  double time_cost;
+
+  void VisitAttrs(tvm::AttrVisitor* v) {
+    v->Visit("filename", &filename);
+    v->Visit("args", &args);
+    v->Visit("error_no", &error_no);
+    v->Visit("error_msg", &error_msg);
+    v->Visit("time_cost", &time_cost);
+  }
+
+  static constexpr const char* _type_key = "meta_schedule.BuildResult";
+  TVM_DECLARE_FINAL_OBJECT_INFO(BuildResultNode, Object);
+};
+
+/*!
+ * \brief Managed reference to BuildResultNode.
+ * \sa BuildResultNode
+ */
+class BuildResult : public ObjectRef {
+ public:
+  /*!
+   * \brief The constructor.
+   * \param filename The filename of built binary file.
+   * \param args The arguments.
+   * \param error_no The error code.
+   * \param error_msg The error message if there is any error.
+   * \param time_cost The time cost of build.
+   */
+  BuildResult(String filename, Array<te::Tensor> args, int error_no, String error_msg,
+              double time_cost);
+  TVM_DEFINE_OBJECT_REF_METHODS(BuildResult, ObjectRef, BuildResultNode);
+};
+
+/********** MeasureResult **********/
+
+class MeasureResult;
+
+/*! \brief Store the results of a measurement. */
+class MeasureResultNode : public Object {
+ public:
+  /*! \brief The time costs of execution. */
+  Array<PrimExpr> costs;
+  /*! \brief The error code. (0 means no error, see MeasureErrorNO) */
+  int error_no;
+  /*! \brief The error message if there is any error. */
+  String error_msg;
+  /*! \brief The time cost of build and run. */
+  double all_cost;
+  /*! \brief The time stamps of this measurement. */
+  double timestamp;
+
+  void VisitAttrs(tvm::AttrVisitor* v) {
+    v->Visit("costs", &costs);
+    v->Visit("error_no", &error_no);
+    v->Visit("error_msg", &error_msg);
+    v->Visit("all_cost", &all_cost);
+    v->Visit("timestamp", &timestamp);
+  }
+
+  /*! \brief Do shallow copy. */
+  MeasureResult copy() const;
+
+  static constexpr const char* _type_key = "meta_schedule.MeasureResult";
+  TVM_DECLARE_FINAL_OBJECT_INFO(MeasureResultNode, Object);
+};
+
+/*!
+ * \brief Managed reference to MeasureResultNode.
+ * \sa MeasureResultNode
+ */
+class MeasureResult : public ObjectRef {
+ public:
+  /*!
+   * \brief The constructor.
+   * \param costs The time costs of execution.
+   * \param error_no The error code.
+   * \param error_msg The error message if there is any error.
+   * \param all_cost The time cost of build and run.
+   * \param timestamp The time stamps of this measurement.
+   */
+  MeasureResult(Array<PrimExpr> costs, int error_no, String error_msg, double all_cost,
+                double timestamp);
+
+  TVM_DEFINE_OBJECT_REF_METHODS(MeasureResult, ObjectRef, MeasureResultNode);
+};
+
+/********** ProgramBuilder **********/
+
+/*! \brief ProgramBuilder that builds the programs */
+class ProgramBuilderNode : public Object {
+ public:
+  /*! \brief The number of build processes to run in parallel */
+  int n_parallel;
+  /*! \brief Timeout of a build */
+  int timeout;
+
+  /*!
+   * \brief Build programs and return results.
+   * \param inputs An Array of MeasureInput.
+   * \param verbose Verbosity level. 0 for silent, 1 to output information during program
+   * building.
+   * \return An Array of MeasureResult.
+   */
+  virtual Array<BuildResult> Build(const Array<MeasureInput>& inputs, int verbose) = 0;
+
+  static constexpr const char* _type_key = "meta_schedule.ProgramBuilder";
+  TVM_DECLARE_BASE_OBJECT_INFO(ProgramBuilderNode, Object);
+};
+
+/*!
+ * \brief Managed reference to ProgramBuilderNode.
+ * \sa ProgramBuilderNode
+ */
+class ProgramBuilder : public ObjectRef {
+ public:
+  TVM_DEFINE_MUTABLE_OBJECT_REF_METHODS(ProgramBuilder, ObjectRef, ProgramBuilderNode);
+};
+
+/********** ProgramRunner **********/
+
+/*! \brief ProgramRunner that runs the built programs and measure the time cost. */
+class ProgramRunnerNode : public Object {
+ public:
+  /*! \brief Timeout of a run. */
+  int timeout;
+  /*! \brief The number of times to run the generated code for taking average. */
+  int number;
+  /*! \brief The number of times to repeat the measurement. */
+  int repeat;
+  /*! \brief The minimum duration of one repeat in milliseconds. */
+  int min_repeat_ms;
+  /*! \brief The cool down interval between two measurements. */
+  double cooldown_interval;
+  /*! \brief Whether to flush cache on CPU between repeated measurements. */
+  bool enable_cpu_cache_flush;
+
+  /*!
+   * \brief Run measurement and return results.
+   * \param inputs An Array of MeasureInput.
+   * \param build_results An Array of BuildResult.
+   * \param verbose Verbosity level. 0 for silent, 1 to output information during program
+   * running.
+   * \return An Array of MeasureResult.
+   */
+  virtual Array<MeasureResult> Run(const Array<MeasureInput>& inputs,
+                                   const Array<BuildResult>& build_results, int verbose) = 0;
+
+  static constexpr const char* _type_key = "meta_schedule.ProgramRunner";
+  TVM_DECLARE_BASE_OBJECT_INFO(ProgramRunnerNode, Object);
+};
+
+/*!
+ * \brief Managed reference to ProgramRunnerNode.
+ * \sa ProgramRunnerNode
+ */
+class ProgramRunner : public ObjectRef {
+ public:
+  TVM_DEFINE_MUTABLE_OBJECT_REF_METHODS(ProgramRunner, ObjectRef, ProgramRunnerNode);
+};
+
+/********** LocalBuilder: ProgramBuilder **********/
+
+/*! \brief LocalBuilder use local CPU cores to build programs in parallel */
+class LocalBuilderNode : public ProgramBuilderNode {
+ public:
+  /*! \brief Build function. */
+  String build_func;
+
+  Array<BuildResult> Build(const Array<MeasureInput>& inputs, int verbose) final;
+
+  static constexpr const char* _type_key = "meta_schedule.LocalBuilder";
+  TVM_DECLARE_FINAL_OBJECT_INFO(LocalBuilderNode, ProgramBuilderNode);
+};
+
+/*!
+ * \brief Managed reference to LocalBuilderNode.
+ * \sa LocalBuilderNode
+ */
+class LocalBuilder : public ProgramBuilder {
+ public:
+  /*!
+   * \brief The constructor.
+   * \param timeout The timeout limit (in second) for each build thread.
+   * This will be used in a wrapper of the multiprocessing.Process.join().
+   * \param n_parallel The number of threads used to build in parallel.
+   * \param build_func The name of the registered build function.
+   */
+  LocalBuilder(int timeout, int n_parallel, const String& build_func);
+
+  TVM_DEFINE_OBJECT_REF_METHODS(LocalBuilder, ProgramBuilder, LocalBuilderNode);
+};
+
+/********** RPCRunner: ProgramRunner **********/
+
+/*!
+ * \brief RPCRunner that uses RPC call to measures the time cost of programs on remote devices.
+ * Or sometime we may need to use RPC even in local running to insulate the thread environment.
+ * (e.g. running CUDA programs)
+ */
+class RPCRunnerNode : public ProgramRunnerNode {
+ public:
+  /*! \brief The key of the device registered in the RPC tracker. */
+  String key;
+  /*! \brief The host address of the RPC Tracker. */
+  String host;
+  /*! \brief The port of the RPC Tracker. */
+  int port;
+  /*! \brief The priority of this run request, larger is more prior. */
+  int priority;
+  /*! \brief The number of tasks run in parallel. */
+  int n_parallel;
+
+  Array<MeasureResult> Run(const Array<MeasureInput>& inputs,
+                           const Array<BuildResult>& build_results, int verbose) final;
+
+  static constexpr const char* _type_key = "meta_schedule.RPCRunner";
+  TVM_DECLARE_FINAL_OBJECT_INFO(RPCRunnerNode, ProgramRunnerNode);
+};
+
+/*!
+ * \brief Managed reference to RPCRunnerNode.
+ * \sa RPCRunnerNode
+ */
+class RPCRunner : public ProgramRunner {
+ public:
+  /*!
+   * \brief The constructor. See the corresponding class in python/tvm/meta_schedule/measure.py
+   * for more detailed parameter explanation.
+   * \param key The key of the device registered in the RPC tracker.
+   * \param host The host address of the RPC Tracker.
+   * \param port The port of RPC Tracker.
+   * \param priority The priority of this run request, larger is more prior.
+   * \param n_parallel The number of tasks run in parallel.
+   * \param timeout Timeout of a run.
+   * \param number The number of times to run the generated code for taking average.
+   * \param repeat The number of times to repeat the measurement.
+   * \param min_repeat_ms The minimum duration of one repeat in milliseconds.
+   * \param cooldown_interval The cool down interval between two measurements.
+   * \param enable_cpu_cache_flush Whether to flush cache on CPU between repeated measurements.
+   */
+  RPCRunner(const String& key, const String& host, int port, int priority, int n_parallel,
+            int timeout, int number, int repeat, int min_repeat_ms, double cooldown_interval,
+            bool enable_cpu_cache_flush);
+
+  TVM_DEFINE_MUTABLE_OBJECT_REF_METHODS(RPCRunner, ProgramRunner, RPCRunnerNode);
+};
+
+}  // namespace meta_schedule
+}  // namespace tvm
+
+#endif  // SRC_META_SCHEDULE_MEASURE_H_
