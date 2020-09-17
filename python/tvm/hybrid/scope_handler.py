74d33fd31ac9c007efd2c2fb71e5ac7a5c4bb426
Bohan Hou <32121147+spectrometerHBH@users.noreply.github.com>
2020-09-17 14:39:28 +0800

    [TIR][Hybrid] concise thread env (#113)
    * [TIR][Hybrid] concise thread env
    
    * [TIR][Hybrid] concise thread env
    
    * [TIR][Hybrid] fix

--- python/tvm/hybrid/scope_handler.py
+++ python/tvm/hybrid/scope_handler.py
@@ -185,6 +185,15 @@ def allocate(parser, node, extents, dtype, scope, condition=True):
 
 
 @register_with_scope(concise=True)
+def launch_thread(parser, node, body, env_var, extent):
+    extent = tvm.runtime.convert(extent)
+    return tvm.tir.AttrStmt(
+        tvm.tir.IterVar(None, env_var, getattr(tvm.tir.IterVar, "ThreadIndex"),
+                        parser.var_env_dict[env_var]),
+        "thread_extent", extent, body)
+
+
+@register_with_scope(concise=True)
 def realize(parser, node, body, buffer_bounds, scope, condition=True):
     """ With scope handler function tir.realize(buffer_bounds, scope, condition) """
     buffer, bounds = buffer_bounds.buffer, buffer_bounds.region
