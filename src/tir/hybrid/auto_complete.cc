5b8e2e69fa52e32e093163819e2d2879a6d52f83
Bohan Hou <32121147+spectrometerHBH@users.noreply.github.com>
2020-09-28 22:46:41 +0800

    [TIR] fix AutoComplete (#133)
    * [TIR] fix auto complete
    
    * [TIR] fix auto complete
    
    * [TIR] fix auto complete

--- src/tir/hybrid/auto_complete.cc
+++ src/tir/hybrid/auto_complete.cc
@@ -22,11 +22,127 @@
  * \brief Used by Hybrid Script parser to expand incomplete TIR input
  */
 
-#include <tvm/tir/stmt_functor.h>
+#include <tvm/arith/int_set.h>
 #include <tvm/runtime/registry.h>
+#include <tvm/tir/stmt_functor.h>
+
 #include <utility>
 
+#include "../schedule/schedule_common.h"
+
 namespace tvm {
+namespace tir {
+
+Array<TensorRegion> BlockReadWriteCollector::reads() {
+  std::vector<TensorRegion> res;
+  for (size_t i = 0; i < read_regions_.size(); ++i) {
+    std::vector<Range> region;
+    for (const auto& range : read_regions_[i])
+      region.push_back(range.CoverRange(Range::FromMinExtent(0, 0)));
+    res.emplace_back(read_buffers_[i], region);
+  }
+  return res;
+}
+
+Array<TensorRegion> BlockReadWriteCollector::writes() {
+  std::vector<TensorRegion> res;
+  for (size_t i = 0; i < write_regions_.size(); ++i) {
+    std::vector<Range> region;
+    for (const auto& range : write_regions_[i])
+      region.push_back(range.CoverRange(Range::FromMinExtent(0, 0)));
+    res.emplace_back(writes_buffers_[i], region);
+  }
+  return res;
+}
+
+void BlockReadWriteCollector::VisitStmt_(const LoopNode* op) {
+  Range range = Range::FromMinExtent(op->min, op->extent);
+  dom_map_[op->loop_var.get()] = arith::IntSet::FromRange(range);
+  StmtVisitor::VisitStmt_(op);
+  dom_map_.erase(op->loop_var.get());
+}
+
+void BlockReadWriteCollector::Update(std::vector<Buffer>* buffers,
+                                     std::vector<std::vector<arith::IntSet>>* regions,
+                                     const Buffer& buffer,
+                                     const std::vector<arith::IntSet>& region) {
+  if (inner_buffers_.find(buffer.get()) != inner_buffers_.end()) return;
+  bool find = false;
+  for (size_t i = 0; i < regions->size(); ++i)
+    if ((*buffers)[i].same_as(buffer)) {
+      find = true;
+      CHECK_EQ((*regions)[i].size(), region.size()) << "Inconsistent buffer dimension";
+      for (size_t j = 0; j < region.size(); ++j) {
+        (*regions)[i][j] = arith::Union({(*regions)[i][j], region[j]});
+      }
+    }
+  if (!find) {
+    buffers->push_back(buffer);
+    regions->push_back(region);
+  }
+}
+
+void BlockReadWriteCollector::VisitExpr_(const BufferLoadNode* op) {
+  std::vector<arith::IntSet> relaxed_region;
+  for (size_t j = 0; j < op->indices.size(); ++j) {
+    relaxed_region.push_back(arith::EvalSet(op->indices[j], dom_map_));
+  }
+  Update(&read_buffers_, &read_regions_, op->buffer, relaxed_region);
+  ExprVisitor::VisitExpr_(op);
+}
+
+void BlockReadWriteCollector::VisitStmt_(const BufferStoreNode* op) {
+  std::vector<arith::IntSet> relaxed_region;
+  for (size_t j = 0; j < op->indices.size(); ++j) {
+    relaxed_region.push_back(arith::EvalSet(op->indices[j], dom_map_));
+  }
+  Update(&writes_buffers_, &write_regions_, op->buffer, relaxed_region);
+  StmtVisitor::VisitStmt_(op);
+}
+
+void BlockReadWriteCollector::VisitStmt_(const ReduceStepNode* op) {
+  const auto* buffer_load = op->lhs.as<BufferLoadNode>();
+  CHECK(buffer_load != nullptr)
+      << "TypeError: 'decompose_reduction' expects the body of the reduce step "
+         "is BufferLoad, but get type: "
+      << op->lhs->GetTypeKey();
+  std::vector<arith::IntSet> relaxed_region;
+  for (size_t j = 0; j < buffer_load->indices.size(); ++j) {
+    relaxed_region.push_back(arith::EvalSet(buffer_load->indices[j], dom_map_));
+  }
+  Update(&writes_buffers_, &write_regions_, buffer_load->buffer, relaxed_region);
+  StmtVisitor::VisitStmt_(op);
+}
+
+void BlockReadWriteCollector::VisitStmt_(const BlockRealizeNode* op) {
+  std::unordered_map<const VarNode*, PrimExpr> vmap;
+  for (size_t i = 0; i < op->block->iter_vars.size(); ++i) {
+    vmap[op->block->iter_vars[i]->var.get()] = op->binding_values[i];
+  }
+  for (const auto& read : op->block->reads) {
+    std::vector<arith::IntSet> relaxed_region;
+    for (const auto& range : read->region) {
+      relaxed_region.push_back(
+          arith::EvalSet(arith::IntSet::FromRange(Range::FromMinExtent(
+                             Substitute(range->min, vmap), Substitute(range->extent, vmap))),
+                         dom_map_));
+    }
+    Update(&read_buffers_, &read_regions_, read->buffer, relaxed_region);
+  }
+  for (const auto& write : op->block->writes) {
+    std::vector<arith::IntSet> relaxed_region;
+    for (const auto& range : write->region) {
+      relaxed_region.push_back(
+          arith::EvalSet(arith::IntSet::FromRange(Range::FromMinExtent(
+                             Substitute(range->min, vmap), Substitute(range->extent, vmap))),
+                         dom_map_));
+    }
+    Update(&writes_buffers_, &write_regions_, write->buffer, relaxed_region);
+  }
+}
+
+}  // namespace tir
+
 namespace tir {
 namespace hybrid {
 
@@ -54,6 +170,20 @@ class AutoCompleter : public StmtMutator {
     }
     return body;
   }
+
+  Stmt VisitStmt_(const BlockNode* op) override {
+    Block block = Downcast<Block>(StmtMutator::VisitStmt_(op));
+    if (!block->reads.defined() || !block->writes.defined()) {
+      BlockReadWriteCollector block_read_write_collector(block->allocations);
+      block_read_write_collector(block->body);
+      auto n = CopyOnWrite(block.operator->());
+      if (!n->reads.defined()) n->reads = block_read_write_collector.reads();
+      if (!n->writes.defined()) n->writes = block_read_write_collector.writes();
+      return Block(n);
+    } else {
+      return std::move(block);
+    }
+  }
 };
 
 TVM_REGISTER_GLOBAL("hybrid.AutoComplete")
@@ -63,10 +193,10 @@ TVM_REGISTER_GLOBAL("hybrid.AutoComplete")
       // generate surrounding loops automatically
       Stmt res = auto_completer(std::move(body));
       // generate root block automatically
-      if (auto_completer.contains_block
-          && (!res->IsInstance<BlockRealizeNode>() || !root_allocates.empty())) {
-          res = Block({}, {}, {}, res, root_allocates, {}, "root");
-          res = BlockRealize({}, Bool(true), Downcast<Block>(res), String(""));
+      if (auto_completer.contains_block &&
+          (!res->IsInstance<BlockRealizeNode>() || !root_allocates.empty())) {
+        res = Block({}, {}, {}, res, root_allocates, {}, "root");
+        res = BlockRealize({}, Bool(true), Downcast<Block>(res), String(""));
       }
       return res;
     });
