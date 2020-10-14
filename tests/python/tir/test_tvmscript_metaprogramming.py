42cf19299b416d2cda120065580f035adf19577f
spectrometerHBH <spectrometer@sjtu.edu.cn>
2020-10-14 21:41:23 +0800

    [TVMScript] metaprogramming: tensor shape invariant code

--- tests/python/tir/test_tvmscript_metaprogramming.py
+++ tests/python/tir/test_tvmscript_metaprogramming.py
@@ -44,7 +44,7 @@ def matmul_128(a: ty.int32, b: ty.int32, c: ty.int32) -> None:
         reducer.step(C[vi, vj], A[vi, vk] * B[vj, vk])
 
 
-def test_tensor_dimension_invariant_code():
+def test_tensor_dimension_invariant_code_matmul():
     m = matmul.attrs["free_vars"][0]
     func1 = matmul.bind({m: 128})  # syntax 1
     tvm.ir.assert_structural_equal(func1, matmul_128)
@@ -53,5 +53,63 @@ def test_tensor_dimension_invariant_code():
     tvm.ir.assert_structural_equal(func2, matmul_128)
 
 
+@tvm.script.tir
+def element_wise(a: ty.handle, c: ty.handle) -> None:
+    m = tir.var("int32")
+    n = tir.var("int32")
+    tir.func_attr({"free_vars": [m, n]})
+    A = tir.match_buffer(a, (m, n), "float32")
+    C = tir.match_buffer(c, (m, n), "float32")
+
+    B = tir.buffer_allocate((m, n), "float32")
+
+    with tir.block([m, n], "B") as [vi, vj]:
+        B[vi, vj] = A[vi, vj] * 2.0
+
+    with tir.block([m, n], "C") as [vi, vj]:
+        C[vi, vj] = B[vi, vj] + 1.0
+
+
+@tvm.script.tir
+def element_wise_128_n(a: ty.handle, c: ty.handle) -> None:
+    n = tir.var("int32")
+    tir.func_attr({"free_vars": [n]})
+    A = tir.match_buffer(a, (128, n), "float32")
+    C = tir.match_buffer(c, (128, n), "float32")
+    B = tir.buffer_allocate((128, n), "float32")
+
+    with tir.block([128, n], "B") as [vi, vj]:
+        B[vi, vj] = A[vi, vj] * 2.0
+
+    with tir.block([128, n], "C") as [vi, vj]:
+        C[vi, vj] = B[vi, vj] + 1.0
+
+
+@tvm.script.tir
+def element_wise_128_64(a: ty.handle, c: ty.handle) -> None:
+    A = tir.match_buffer(a, (128, 64), "float32")
+    C = tir.match_buffer(c, (128, 64), "float32")
+    B = tir.buffer_allocate((128, 64), "float32")
+
+    with tir.block([128, 64], "B") as [vi, vj]:
+        B[vi, vj] = A[vi, vj] * 2.0
+
+    with tir.block([128, 64], "C") as [vi, vj]:
+        C[vi, vj] = B[vi, vj] + 1.0
+
+
+def test_tensor_dimension_invariant_code_elemwise():
+    m, n = element_wise.attrs["free_vars"]
+    func1 = element_wise.bind({m: 128, n: 64})  # syntax 1
+    tvm.ir.assert_structural_equal(func1, element_wise_128_64)
+
+    func2 = element_wise.bind([128, 64])  # syntax 2
+    tvm.ir.assert_structural_equal(func2, element_wise_128_64)
+
+    func3 = element_wise.bind({m: 128})  # syntax 3: partially bind
+    tvm.ir.assert_structural_equal(func3, element_wise_128_n)
+
+
 if __name__ == '__main__':
-    test_tensor_dimension_invariant_code()
+    test_tensor_dimension_invariant_code_matmul()
+    test_tensor_dimension_invariant_code_elemwise()
