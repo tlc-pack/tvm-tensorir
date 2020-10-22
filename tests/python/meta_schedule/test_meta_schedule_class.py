32e948a0a8e052b23bfe66ecef0d62808807a05e
Junru Shao <junrushao1994@gmail.com>
2020-10-22 07:45:33 -0700

    [Refactor] Workloads in meta schedule testcases (#166)

--- tests/python/meta_schedule/test_meta_schedule_class.py
+++ tests/python/meta_schedule/test_meta_schedule_class.py
@@ -22,32 +22,10 @@ import tvm
 from tvm import meta_schedule as ms
 from tvm import tir
 from tvm.script import ty
+from tir_workload import matmul, matmul_relu
 
-# pylint: disable=invalid-name,no-member,unused-variable
-
-
-@tvm.script.tir
-def matmul(a: ty.handle, b: ty.handle, c: ty.handle) -> None:
-    A = tir.match_buffer(a, (1024, 1024), "float32")
-    B = tir.match_buffer(b, (1024, 1024), "float32")
-    C = tir.match_buffer(c, (1024, 1024), "float32")
-    reducer = tir.comm_reducer(lambda x, y: x + y, tir.float32(0))
-    with tir.block([1024, 1024, tir.reduce_axis(0, 1024)], "C") as [vi, vj, vk]:
-        reducer.step(C[vi, vj], A[vi, vk] * B[vk, vj])
-
-
-@tvm.script.tir
-def matmul_relu(a: ty.handle, b: ty.handle, d: ty.handle) -> None:
-    A = tir.match_buffer(a, (1024, 1024), "float32")
-    B = tir.match_buffer(b, (1024, 1024), "float32")
-    D = tir.match_buffer(d, (1024, 1024), "float32")
-    C = tir.buffer_allocate((1024, 1024), "float32")
-    reducer = tir.comm_reducer(lambda x, y: x + y, tir.float32(0))
-    with tir.block([1024, 1024, tir.reduce_axis(0, 1024)], "matmul") as [vi, vj, vk]:
-        reducer.step(C[vi, vj], A[vi, vk] * B[vk, vj])
-    with tir.block([1024, 1024], "relu") as [vi, vj]:
-        D[vi, vj] = tir.max(C[vi, vj], 0.0)
-
+# pylint: disable=invalid-name,no-member,line-too-long,too-many-nested-blocks
+# fmt: off
 
 @tvm.script.tir
 def matmul_blockized(a: ty.handle, b: ty.handle, c: ty.handle) -> None:
@@ -63,38 +41,26 @@ def matmul_blockized(a: ty.handle, b: ty.handle, c: ty.handle) -> None:
         tir.writes([])
         for i0 in range(0, 1024):
             for i1 in range(0, 1024):
-                with tir.block([1024, 1024, tir.reduce_axis(0, 1024)], "blockized_C") as [
-                    vi,
-                    vj,
-                    vk,
-                ]:
+                with tir.block([1024, 1024, tir.reduce_axis(0, 1024)], "blockized_C") as [vi, vj, vk]:
                     tir.bind(vi, i0)
                     tir.bind(vj, i1)
                     tir.bind(vk, 0)
-                    tir.reads(
-                        [
-                            C[vi : (vi + 1), vj : (vj + 1)],
-                            A[vi : (vi + 1), 0:1024],
-                            B[0:1024, vj : (vj + 1)],
-                        ]
-                    )
+                    tir.reads([
+                        C[vi : (vi + 1), vj : (vj + 1)],
+                        A[vi : (vi + 1), 0:1024],
+                        B[0:1024, vj : (vj + 1)],
+                    ])
                     tir.writes([C[vi : (vi + 1), vj : (vj + 1)]])
                     for i2 in range(0, 1024):
-                        with tir.block([1024, 1024, tir.reduce_axis(0, 1024)], "C") as [
-                            vi_1,
-                            vj_1,
-                            vk_1,
-                        ]:
+                        with tir.block([1024, 1024, tir.reduce_axis(0, 1024)], "C") as [vi_1, vj_1, vk_1]:
                             tir.bind(vi_1, vi)
                             tir.bind(vj_1, vj)
                             tir.bind(vk_1, i2)
-                            tir.reads(
-                                [
-                                    C[vi_1 : (vi_1 + 1), vj_1 : (vj_1 + 1)],
-                                    A[vi_1 : (vi_1 + 1), vk_1 : (vk_1 + 1)],
-                                    B[vk_1 : (vk_1 + 1), vj_1 : (vj_1 + 1)],
-                                ]
-                            )
+                            tir.reads([
+                                C[vi_1 : (vi_1 + 1), vj_1 : (vj_1 + 1)],
+                                A[vi_1 : (vi_1 + 1), vk_1 : (vk_1 + 1)],
+                                B[vk_1 : (vk_1 + 1), vj_1 : (vj_1 + 1)],
+                            ])
                             tir.writes([C[vi_1 : (vi_1 + 1), vj_1 : (vj_1 + 1)]])
                             reducer.step(C[vi_1, vj_1], (A[vi_1, vk_1] * B[vk_1, vj_1]))
 
@@ -115,25 +81,19 @@ def matmul_relu_fused(a: ty.handle, b: ty.handle, d: ty.handle) -> None:
         for i0 in range(0, 1024):
             for i1 in range(0, 1024):
                 for i2 in range(0, 1024):
-                    with tir.block([1024, 1024, tir.reduce_axis(0, 1024)], "matmul") as [
-                        vi,
-                        vj,
-                        vk,
-                    ]:
+                    with tir.block([1024, 1024, tir.reduce_axis(0, 1024)], "matmul") as [vi, vj, vk]:
                         tir.bind(vi, i0)
                         tir.bind(vj, i1)
                         tir.bind(vk, i2)
-                        tir.reads(
-                            [
-                                C[vi : (vi + 1), vj : (vj + 1)],
-                                A[vi : (vi + 1), vk : (vk + 1)],
-                                B[vk : (vk + 1), vj : (vj + 1)],
-                            ]
-                        )
+                        tir.reads([
+                            C[vi : (vi + 1), vj : (vj + 1)],
+                            A[vi : (vi + 1), vk : (vk + 1)],
+                            B[vk : (vk + 1), vj : (vj + 1)],
+                        ])
                         tir.writes([C[vi : (vi + 1), vj : (vj + 1)]])
                         reducer.step(C[vi, vj], (A[vi, vk] * B[vk, vj]))
-                for ax0 in range(0, 1):
-                    for ax1 in range(0, 1):
+                for ax0 in range(0, 1):  # pylint: disable=unused-variable
+                    for ax1 in range(0, 1):  # pylint: disable=unused-variable
                         with tir.block([1024, 1024], "relu") as [vi_1, vj_1]:
                             tir.bind(vi_1, i0)
                             tir.bind(vj_1, i1)
@@ -154,27 +114,19 @@ def matmul_cache_write(a: ty.handle, b: ty.handle, c: ty.handle) -> None:
     with tir.block([], "root") as []:
         tir.reads([])
         tir.writes([])
-        C_local = tir.buffer_allocate(
-            [1024, 1024], elem_offset=0, scope="local", align=128, offset_factor=1
-        )
+        C_local = tir.buffer_allocate([1024, 1024], elem_offset=0, scope="local", align=128, offset_factor=1)
         for i0 in range(0, 1024):
             for i1 in range(0, 1024):
                 for i2 in range(0, 1024):
-                    with tir.block([1024, 1024, tir.reduce_axis(0, 1024)], "C") as [
-                        vi,
-                        vj,
-                        vk,
-                    ]:
+                    with tir.block([1024, 1024, tir.reduce_axis(0, 1024)], "C") as [vi, vj, vk]:
                         tir.bind(vi, i0)
                         tir.bind(vj, i1)
                         tir.bind(vk, i2)
-                        tir.reads(
-                            [
-                                C_local[vi : (vi + 1), vj : (vj + 1)],
-                                A[vi : (vi + 1), vk : (vk + 1)],
-                                B[vk : (vk + 1), vj : (vj + 1)],
-                            ]
-                        )
+                        tir.reads([
+                            C_local[vi : (vi + 1), vj : (vj + 1)],
+                            A[vi : (vi + 1), vk : (vk + 1)],
+                            B[vk : (vk + 1), vj : (vj + 1)],
+                        ])
                         tir.writes([C_local[vi : (vi + 1), vj : (vj + 1)]])
                         reducer.step(C_local[vi, vj], (A[vi, vk] * B[vk, vj]))
         for ax0 in range(0, 1024):
@@ -206,7 +158,8 @@ def elementwise_inlined(a: ty.handle, c: ty.handle) -> None:
         C[vi, vj] = (A[vi, vj] + 1.0) * 2.0
 
 
-# pylint: enable=invalid-name,no-member,unused-variable
+# fmt: on
+# pylint: enable=invalid-name,no-member,line-too-long,too-many-nested-blocks
 
 
 def test_meta_schedule_creation():
@@ -217,7 +170,7 @@ def test_meta_schedule_creation():
 
 def test_meta_schedule_copy():
     sch = ms.Schedule(func=matmul)
-    i, j, k = sch.get_axes(sch.get_block("C"))
+    i, j, k = sch.get_axes(sch.get_block("matmul"))
     sch_copy = sch.copy(seed=42)
     assert not sch.evaluate(i).same_as(sch_copy.evaluate(i))
     assert not sch.evaluate(j).same_as(sch_copy.evaluate(j))
@@ -248,7 +201,7 @@ def test_meta_schedule_sample_tile_factor():
 
     where = [1, 2, 4]
     sch = ms.Schedule(func=matmul)
-    i, _, _ = sch.get_axes(sch.get_block("C"))
+    i, _, _ = sch.get_axes(sch.get_block("matmul"))
     factors = sch.sample_tile_factor(n_splits=4, loop=i, where=where)
     factors = [sch.evaluate(i) for i in factors]
     for i in factors[1:]:
@@ -261,7 +214,7 @@ def test_meta_schedule_sample_perfect_tile():
     from functools import reduce  # pylint: disable=import-outside-toplevel
 
     sch = ms.Schedule(func=matmul)
-    i, _, _ = sch.get_axes(sch.get_block("C"))
+    i, _, _ = sch.get_axes(sch.get_block("matmul"))
     factors = sch.sample_perfect_tile(n_splits=4, loop=i)
     factors = [sch.evaluate(i) for i in factors]
     prod = reduce(lambda x, y: x * y, factors)
@@ -270,7 +223,7 @@ def test_meta_schedule_sample_perfect_tile():
 
 def test_meta_schedule_sample_fusible_loops():
     sch = ms.Schedule(func=matmul)
-    loops = sch.get_axes(block=sch.get_block(name="C"))
+    loops = sch.get_axes(block=sch.get_block("matmul"))
     result = sch.sample_fusible_loops(
         loops=loops,
         loop_types=[0, 0, 0],
@@ -317,7 +270,7 @@ def test_meta_schedule_get_only_consumer():
 
 def test_meta_schedule_get_block():
     sch = ms.Schedule(func=matmul)
-    block = sch.get_block("C")
+    block = sch.get_block("matmul")
     assert tvm.ir.structural_equal(
         sch.evaluate(block).stmt,
         matmul.body.block.body.body.body.body.block,
@@ -326,7 +279,7 @@ def test_meta_schedule_get_block():
 
 def test_meta_schedule_get_axes():
     sch = ms.Schedule(func=matmul)
-    block = sch.get_block("C")
+    block = sch.get_block("matmul")
     axes = sch.get_axes(block)
     i_0, i_1, i_2 = [sch.evaluate(i).stmt for i in axes]
     assert tvm.ir.structural_equal(i_0, matmul.body.block.body)
@@ -354,7 +307,7 @@ def test_meta_schedule_get_leaf_blocks():
 
 def test_meta_schedule_fuse():
     sch = ms.Schedule(func=matmul)
-    block = sch.get_block(name="C")
+    block = sch.get_block("matmul")
     i, j, _ = sch.get_axes(block)
     sch.fuse(loops=[i, j])
     assert len(sch.get_axes(block)) == 2
@@ -369,10 +322,10 @@ def test_meta_schedule_mark_parallel():
         assert ann.value == "lazy_parallel"
 
     sch = ms.Schedule(func=matmul)
-    block = sch.get_block(name="C")
+    block = sch.get_block("matmul")
     axes = sch.get_axes(block)
     sch.mark_parallel(axes, tvm.ir.Range(0, 3))
-    block = sch.get_block(name="C")
+    block = sch.get_block("matmul")
     i, j, k = sch.get_axes(block)
     check_annotation(sch, i)
     check_annotation(sch, j)
@@ -388,10 +341,10 @@ def test_meta_schedule_mark_vectorize():
         assert ann.value == "lazy_vectorize"
 
     sch = ms.Schedule(func=matmul)
-    block = sch.get_block(name="C")
+    block = sch.get_block(name="matmul")
     axes = sch.get_axes(block)
     sch.mark_vectorize(axes, tvm.ir.Range(0, 3))
-    block = sch.get_block(name="C")
+    block = sch.get_block(name="matmul")
     i, j, k = sch.get_axes(block)
     check_annotation(sch, i)
     check_annotation(sch, j)
@@ -400,7 +353,7 @@ def test_meta_schedule_mark_vectorize():
 
 def test_meta_schedule_split():
     sch = ms.Schedule(func=matmul)
-    i, _, _ = sch.get_axes(sch.get_block("C"))
+    i, _, _ = sch.get_axes(sch.get_block("matmul"))
     i_0, i_1, i_2 = [sch.evaluate(i).stmt for i in sch.split(loop=i, factors=[4, 8, 32])]
     assert tvm.ir.structural_equal(i_0, sch.sch.func.body.block.body)
     assert tvm.ir.structural_equal(i_1, sch.sch.func.body.block.body.body)
@@ -409,12 +362,12 @@ def test_meta_schedule_split():
 
 def test_meta_schedule_reorder():
     sch = ms.Schedule(func=matmul)
-    i_0, i_1, i_2 = sch.get_axes(sch.get_block("C"))
+    i_0, i_1, i_2 = sch.get_axes(sch.get_block("matmul"))
     sch.reorder(after_axes=[i_2, i_1, i_0])
     i_0, i_1, i_2 = [sch.evaluate(i).stmt for i in [i_0, i_1, i_2]]
 
     tir_sch = tir.create_schedule(func=matmul)
-    ti_0, ti_1, ti_2 = tir_sch.get_axes(tir_sch.get_block("C"))
+    ti_0, ti_1, ti_2 = tir_sch.get_axes(tir_sch.get_block("matmul"))
     tir_sch.reorder(ti_2, ti_1, ti_0)
 
     assert tvm.ir.structural_equal(i_0, ti_0.stmt)
@@ -440,13 +393,13 @@ def test_meta_schedule_compute_inline():
 
 def test_meta_schedule_cache_write():
     sch = ms.Schedule(func=matmul)
-    sch.cache_write(sch.get_block("C"), storage_scope="local")
+    sch.cache_write(sch.get_block("matmul"), storage_scope="local")
     assert tvm.ir.structural_equal(sch.sch.func, matmul_cache_write)
 
 
 def test_meta_schedule_blockize():
     sch = ms.Schedule(func=matmul)
-    block = sch.get_block("C")
+    block = sch.get_block("matmul")
     _, _, k = sch.get_axes(block)
     sch.blockize(k)
     assert tvm.ir.structural_equal(sch.sch.func, matmul_blockized)
@@ -454,7 +407,7 @@ def test_meta_schedule_blockize():
 
 def test_meta_schedule_mutate_decision():
     sch = ms.Schedule(func=matmul)
-    i, j, _ = sch.get_axes(sch.get_block("C"))
+    i, j, _ = sch.get_axes(sch.get_block("matmul"))
     sch.sample_perfect_tile(n_splits=4, loop=i)
     sch.sample_perfect_tile(n_splits=3, loop=j)
     i_inst = sch.trace[-2]
@@ -469,7 +422,7 @@ def test_meta_schedule_resample():
     from functools import reduce  # pylint: disable=import-outside-toplevel
 
     sch = ms.Schedule(func=matmul)
-    i, j, _ = sch.get_axes(sch.get_block("C"))
+    i, j, _ = sch.get_axes(sch.get_block("matmul"))
     i_tiles = sch.sample_perfect_tile(n_splits=4, loop=i)
     j_tiles = sch.sample_perfect_tile(n_splits=3, loop=j)
     i_inst = sch.trace[-2]
@@ -490,7 +443,7 @@ def test_meta_schedule_replay_decision():
     from collections import defaultdict  # pylint: disable=import-outside-toplevel
 
     sch = ms.Schedule(func=matmul)
-    i, j, _ = sch.get_axes(sch.get_block("C"))
+    i, j, _ = sch.get_axes(sch.get_block("matmul"))
     i_tiles = sch.sample_perfect_tile(n_splits=4, loop=i)
     j_tiles = sch.sample_perfect_tile(n_splits=3, loop=j)
     i_inst = sch.trace[-2]
