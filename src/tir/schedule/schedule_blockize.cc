afcdde0e8b357b55747bda1f5e19bc3ff371258b
Bohan Hou <32121147+spectrometerHBH@users.noreply.github.com>
2020-11-06 13:30:49 +0800

    [TIR][Schedule] rfactor (#180)
    * [TIR][Schedule] rfactor : init
    
    * [TIR][Schedule] rfactor
    
    * [TIR][Schedule] rfactor
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * basically done
    
    * DONE.
    
    * DONE.

--- src/tir/schedule/schedule_blockize.cc
+++ src/tir/schedule/schedule_blockize.cc
@@ -194,8 +194,8 @@ StmtSRef ScheduleNode::blockize(const StmtSRef& loop_sref, const String& exec_sc
       BlockRealize(outer_bindings, division.back()->outer_extent, outer_block, exec_scope);
 
   this->Replace(loop_sref, outer_realize, {{inner_block, block}});
-  UpdateScope(GetParentBlockSRef(this->stmt2ref.at(outer_block.get()))->stmt,
-              this->stmt2ref, &this->scopes);
+  UpdateScope(GetParentBlockSRef(this->stmt2ref.at(outer_block.get()))->stmt, this->stmt2ref,
+              &this->scopes);
   UpdateScope(outer_block.get(), this->stmt2ref, &this->scopes);
 
   // Check loop binding
