4e35ac81bc6988ebc0c7541f117357a9f3f0270f
Junru Shao <junrushao1994@gmail.com>
2020-11-23 15:58:49 -0800

    [Testing] Add operator workloads (#192)

--- src/target/tag.cc
+++ src/target/tag.cc
@@ -329,4 +329,22 @@ TVM_REGISTER_CUDA_TAG("nvidia/jetson-tx1", "sm_53", 49152, 32768);
 TVM_REGISTER_CUDA_TAG("nvidia/tegra-x1", "sm_53", 49152, 32768);
 
 #undef TVM_REGISTER_CUDA_TAG
+TVM_REGISTER_TARGET_TAG("raspberry-pi/4b")
+    .set_config({
+        {"kind", String("llvm")},
+        {"mtriple", String("armv8l-linux-gnueabihf")},
+        {"mcpu", String("cortex-a72")},
+        {"mattr", Array<String>{"+neon"}},
+        {"num_cores", Integer(4)},
+    });
+
+TVM_REGISTER_TARGET_TAG("raspberry-pi/4b-64")
+    .set_config({
+        {"kind", String("llvm")},
+        {"mtriple", String("aarch64-linux-gnu")},
+        {"mcpu", String("cortex-a72")},
+        {"mattr", Array<String>{"+neon"}},
+        {"num_cores", Integer(4)},
+    });
+
 }  // namespace tvm
