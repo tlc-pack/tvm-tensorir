9a4ef8c12869a79e240411828a268c4c6ef6a949
Junru Shao <junrushao1994@gmail.com>
2020-11-23 15:58:49 -0800

    [Testing] Add operator workloads (#192)

--- src/target/target_kind.cc
+++ src/target/target_kind.cc
@@ -213,6 +213,7 @@ TVM_REGISTER_TARGET_KIND("llvm", kDLCPU)
     .add_attr_option<String>("mfloat-abi")
     .add_attr_option<Bool>("system-lib")
     .add_attr_option<String>("runtime")
+    .add_attr_option<Integer>("num_cores")
     .set_default_keys({"cpu"});
 
 TVM_REGISTER_TARGET_KIND("c", kDLCPU)
