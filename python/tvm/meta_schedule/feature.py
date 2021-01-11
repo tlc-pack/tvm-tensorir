e57eced7d77b9a7a76e9719b605ede2c251671f8
Junru Shao <junrushao1994@gmail.com>
2021-01-10 19:23:01 -0800

    [MetaSchedule] Feature Extraction (#210)

---
+++ python/tvm/meta_schedule/feature.py
@@ -0,0 +1,47 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+"""Feature extraction interface"""
+
+from ..runtime.ndarray import NDArray
+from . import _ffi_api
+from .schedule import Schedule
+
+
+def calc_per_block_feature(sch: Schedule, max_num_buffer_access_features: int = 5) -> NDArray:
+    """Calculate the per-block feature
+
+    Parameters
+    ----------
+    sch : Schedule
+        The meta schedule class
+    max_num_buffer_access_features : int
+        The maximum number of buffer accesses
+
+    Returns
+    -------
+    scores: List[float]
+        The predicted scores for all schedules
+    """
+    return _ffi_api.CalcPerBlockFeature(  # pylint: disable=no-member
+        sch, max_num_buffer_access_features
+    ).asnumpy()
+
+
+def per_bloc_feature_names(max_num_buffer_access_features: int = 5) -> NDArray:
+    return _ffi_api.PerBlockFeatureNames(  # pylint: disable=no-member
+        max_num_buffer_access_features
+    )
