7562d8d6bd0e3e4fc928ab8a9222e14f95111073
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2021-04-15 23:01:18 -0400

    Readded the checks in the shared stage.

--- python/tvm/contrib/nvcc.py
+++ python/tvm/contrib/nvcc.py
@@ -100,15 +100,15 @@ def compile_cuda(code, target="ptx", arch=None, options=None, path_target=None):
     proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
     # <bojian/TVM-SymbolicTuning>
-    # (out, _) = proc.communicate()
-    proc.wait()
-    print("Compilation has been completed")
+    (out, _) = proc.communicate()
+    # proc.wait()
+    # print("Compilation has been completed")
 
     if proc.returncode != 0:
         msg = code
         msg += "\nCompilation error:\n"
         # <bojian/TVM-SymbolicTuning>
-        # msg += py_str(out)
+        msg += py_str(out)
         raise RuntimeError(msg)
 
     data = bytearray(open(file_target, "rb").read())
