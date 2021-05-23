97f7b06310df5c6ad65f68413c7cc2a5bf6983e7
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2021-05-22 22:37:32 -0400

    Add cuda_space

--- DietCode_Workspace/test/ops/shared/meta.py
+++ DietCode_Workspace/test/ops/shared/meta.py
@@ -32,7 +32,38 @@ def cpu_space():
             )
 
 
-
+def cuda_space():
+    return ms.space.PostOrderApply(
+            stages=[
+                ms.rule.multi_level_tiling(
+                    structure="SSSRRSRS",
+                    must_cache_read=True,
+                    cache_read_scope="shared",
+                    can_cache_write=True,
+                    must_cache_write=True,
+                    cache_write_scope="local",
+                    consumer_inline_strict=False,
+                    fusion_levels=[3],
+                    vector_load_max_len=4,
+                    tile_binds=["blockIdx.x", "vthread", "threadIdx.x"],
+                ),
+                ms.rule.inline_pure_spatial(strict_mode=False),
+                ms.rule.parallelize_vectorize_unroll(
+                    max_jobs_per_core=-1,  # disable parallelize
+                    max_vectorize_extent=-1,  # disable vectorize
+                    unroll_max_steps=[0, 16, 64, 512, 1024],
+                    unroll_explicit=True,
+                ),
+            ],
+            postprocs=[
+                ms.postproc.rewrite_cooperative_fetch(),
+                ms.postproc.rewrite_unbound_blocks(),
+                ms.postproc.rewrite_parallel_vectorize_unroll(),
+                ms.postproc.rewrite_reduction_block(),
+                ms.postproc.disallow_dynamic_loops(),
+                ms.postproc.verify_gpu_code(),
+            ],
+            )
 
 
 def measurer():
