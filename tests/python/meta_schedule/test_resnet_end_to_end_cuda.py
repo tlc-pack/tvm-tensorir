c96aa0bd41f64d27252053819b039ed386537f15
jinhongyi <3231950289@qq.com>
2021-05-31 21:10:39 +0800

    fix

--- tests/python/meta_schedule/test_resnet_end_to_end_cuda.py
+++ tests/python/meta_schedule/test_resnet_end_to_end_cuda.py
@@ -14,7 +14,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-"""End to end resnet-18 CPU test"""
+"""End to end resnet-18 GPU test"""
 # pylint: disable=missing-function-docstring
 import os
 
