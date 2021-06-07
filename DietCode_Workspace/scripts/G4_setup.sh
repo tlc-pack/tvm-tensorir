5c3a877b93e7cd3c5909ab3dc30e5f693f2daad0
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2021-06-07 17:21:36 +0000

    Migrate the G4 setup scripts

---
+++ DietCode_Workspace/scripts/G4_setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+LLVM_VERSION=12
+
+wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add - && \
+        apt-add-repository "deb http://apt.llvm.org/bionic/ llvm-toolchain-bionic-${LLVM_VERSION} main"
+
+apt-get update && \
+apt-get install -y --no-install-recommends \
+        llvm-${LLVM_VERSION} \
+        llvm-${LLVM_VERSION}-dev clang-${LLVM_VERSION} && \
+ln -s /usr/lib/llvm-${LLVM_VERSION} /usr/lib/llvm
+
+apt-get install -y --no-install-recommends \
+        libtinfo-dev libedit-dev libxml2-dev zlib1g-dev
+
+pip3 install numpy scipy decorator attrs psutil typed_ast \
+             cython six xgboost tornado networkx pytest synr
