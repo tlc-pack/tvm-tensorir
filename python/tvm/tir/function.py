b0228064bbebda87d412e901b8802ebce8456835
Siyuan Feng <hzfengsy@sjtu.edu.cn>
2021-06-20 13:22:54 +0000

    specialize

--- python/tvm/tir/function.py
+++ python/tvm/tir/function.py
@@ -16,12 +16,14 @@
 # under the License.
 """Function data types."""
 
+from typing import Union
+
 import tvm._ffi
 import tvm.runtime
 from tvm.runtime import Object
 from tvm.ir import BaseFunc
 from .buffer import Buffer
-from .expr import Var
+from .expr import Var, PrimExpr
 from . import _ffi_api
 
 
@@ -85,3 +87,18 @@ class PrimFunc(BaseFunc):
             The created new function.
         """
         return PrimFunc(self.params, new_body, self.ret_type, self.buffer_map, self.attrs, span)
+
+    def specialize(self, var: Var, instance: Union[PrimExpr, Buffer]):
+        """Metaprogramming usage: specialize parameters of PrimFunc
+        Parameters
+        ----------
+        var: Var
+            The var in function's params
+        instance : Union[PrimExpr, Buffer]
+            The instance we use to specialize the PrimFunc
+        Returns
+        -------
+        new_func : PrimFunc
+            The new function with parameter specialized
+        """
+        return _ffi_api.Specialize(self, var, tvm.runtime.convert(instance))
