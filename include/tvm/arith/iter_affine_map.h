dbc6be9712953c892eb8f1cf9e70e343e8cf483b
jinhongyi <3231950289@qq.com>
2021-07-06 20:02:33 +0800

    address comments

--- include/tvm/arith/iter_affine_map.h
+++ include/tvm/arith/iter_affine_map.h
@@ -283,14 +283,14 @@ Array<IterSumExpr> DetectIterMap(const Array<PrimExpr>& indices, const Map<Var, 
                                  const PrimExpr& predicate, bool require_bijective,
                                  arith::Analyzer* analyzer);
 /*!
- * \brief Use IterVarMap detector to rewrite and simplify the bindings
+ * \brief Use IterVarMap detector to rewrite and simplify the indices
  *
  * \param indices The indices to detect pattern for.
  * \param input_iters Map from variable to iterator's range.
  * \param input_pred The predicate constraints on the input iterators
  * \param require_bijective A boolean flag that indicates whether the mapping should be bijective.
  *
- * \return The bindings after rewrite
+ * \return The indices after rewrite
  */
 Array<PrimExpr> IterMapSimplify(const Array<PrimExpr>& indices, const Map<Var, Range>& input_iters,
                                 const PrimExpr& input_pred, bool require_bijective);
