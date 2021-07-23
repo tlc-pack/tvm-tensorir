08fda1a748e0983a9bef1f72899a51bdd21acd15
spectrometerHBH <spectrometer@sjtu.edu.cn>
2021-07-23 06:10:47 +0000

    fix

--- tests/python/tir/test_schedule_software_pipeline.py
+++ tests/python/tir/test_schedule_software_pipeline.py
@@ -9,14 +9,14 @@ import util
 
 def test_cuda_pipeline():
     device = "cuda"
-    ctx = tvm.context(device, 0)
-    if not ctx.exist:
+    dev = tvm.device(device, 0)
+    if not dev.exist:
         print("Skip because %s is not enabled" % device)
         return
 
     s = tir.Schedule(util.matmul_stmt())
     C = s.get_block('update')
-    i, j, k = s.get_axes(C)
+    i, j, k = s.get_loops(C)
     io, ii = s.split(i, factors=[8, 16])
     jo, ji = s.split(j, factors=[8, 16])
     ko, ki = s.split(k, factors=[8, 16])
@@ -30,16 +30,16 @@ def test_cuda_pipeline():
     s.compute_at(B_shared, ko)
 
     for load in [A_shared, B_shared]:
-        _, tt = s.split(s.fuse(s.get_axes(load)[-2]), factors=[None, 8])
+        _, tt = s.split(s.fuse(s.get_loops(load)[-2]), factors=[None, 8])
         s.bind(tt, 'threadIdx.x')
     s.software_pipeline(ko, 2)
 
     f = tvm.build(s.mod['main'], None, target='cuda')
     a_np = np.random.uniform(size=(128, 128)).astype("float32")
     b_np = np.random.uniform(size=(128, 128)).astype("float32")
-    a = tvm.nd.array(a_np, ctx=ctx)
-    b = tvm.nd.array(b_np, ctx=ctx)
-    c = tvm.nd.array(np.zeros((128, 128), dtype="float32"), ctx=ctx)
+    a = tvm.nd.array(a_np, ctx=dev)
+    b = tvm.nd.array(b_np, ctx=dev)
+    c = tvm.nd.array(np.zeros((128, 128), dtype="float32"), ctx=dev)
     f(a, b, c)
     c_np = np.matmul(a_np, b_np.T)
     tvm.testing.assert_allclose(c.asnumpy(), c_np, rtol=1e-4, atol=1e-4)
