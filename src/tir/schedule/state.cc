1ab5a6b54cb9e6307c892221f69305a0446258a0
Junru Shao <junrushao1994@gmail.com>
2021-09-07 04:05:49 +0000

    address comments

--- src/tir/schedule/state.cc
+++ src/tir/schedule/state.cc
@@ -35,10 +35,10 @@ using SMap = std::unordered_map<K, V, ObjectPtrHash, ObjectPtrEqual>;
  * \param dom_high_exclusive The highest node in the sref tree path
  * \return An n-dimensional integer set
  */
-Array<arith::IntSet> AnalyzeRegionUpperBound(const BufferRegion& region,         //
-                                             const PrimExpr& predicate,          //
-                                             const StmtSRef& dom_low_inclusive,  //
-                                             const StmtSRef& dom_high_exclusive,
+Array<arith::IntSet> AnalyzeRegionUpperBound(const BufferRegion& region,          //
+                                             const PrimExpr& predicate,           //
+                                             const StmtSRef& dom_low_inclusive,   //
+                                             const StmtSRef& dom_high_exclusive,  //
                                              arith::Analyzer* analyzer) {
   Map<Var, Range> var_dom = LoopDomainOfSRefTreePath(
       /*low_inclusive=*/dom_low_inclusive,
