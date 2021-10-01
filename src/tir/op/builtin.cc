4ff3bcae6be36f48d3a2873e8a20c391a2f25b6e
Zihao Ye <expye@outlook.com>
2021-10-01 01:07:39 -0700

    upd

--- src/tir/op/builtin.cc
+++ src/tir/op/builtin.cc
@@ -222,6 +222,9 @@ TIR_DEFINE_BUILTIN_FUNC(tvm_thread_allreduce)
 TIR_DEFINE_BUILTIN_FUNC(tvm_load_matrix_sync)
     .set_attr<TCallEffectKind>("TCallEffectKind", Integer(CallEffectKind::kReadState));
 
+TIR_DEFINE_BUILTIN_FUNC(tvm_lower_bound)
+    .set_attr<TCallEffectKind>("TCallEffectKind", Integer(CallEffectKind::kOpaque));
+
 TIR_DEFINE_BUILTIN_FUNC(tvm_mma_sync)
     .set_attr<TCallEffectKind>("TCallEffectKind", Integer(CallEffectKind::kOpaque));
 
@@ -239,10 +242,6 @@ TIR_DEFINE_BUILTIN_FUNC(vectorhigh)
 
 TIR_DEFINE_BUILTIN_FUNC(vectorlow).set_attr<TCallEffectKind>("TCallEffectKind",
                                                              Integer(CallEffectKind::kPure));
-
-TIR_DEFINE_BUILTIN_FUNC(tvm_lower_bound)
-    .set_num_inputs(3)
-    .set_attr<TCallEffectKind>("TCallEffectKind", Integer(CallEffectKind::kPure));
 
 TIR_DEFINE_BUILTIN_FUNC(vectorcombine)
     .set_attr<TCallEffectKind>("TCallEffectKind", Integer(CallEffectKind::kPure));
