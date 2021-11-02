58fe9423fa5efed05f7e617d6d740fa0ad80e0e7
spectrometerHBH <spectrometer@sjtu.edu.cn>
2021-11-02 17:07:29 -0400

    test

--- tests/python/unittest/test_tir_schedule_get_blocks.py
+++ tests/python/unittest/test_tir_schedule_get_blocks.py
@@ -42,7 +42,14 @@ def test_get_child_blocks():
     s = tir.Schedule(matmul, debug_mask="all")
     init = s.get_block("init")
     update = s.get_block("update")
+    # loop
     blocks = s.get_child_blocks(s.get_loops(init)[0])
+    assert len(blocks) == 2
+    assert s.get(init) == s.get(blocks[0])
+    assert s.get(update) == s.get(blocks[1])
+    # block
+    root = s.get_block("root")
+    blocks = s.get_child_blocks(root)
     assert len(blocks) == 2
     assert s.get(init) == s.get(blocks[0])
     assert s.get(update) == s.get(blocks[1])
