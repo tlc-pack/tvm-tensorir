9a3b851293974d6f88f4942b228cd617d335fdaa
Junru Shao <junrushao1994@gmail.com>
2021-11-28 19:16:37 -0800

    [Meta Schedule] Per-Store-Feature (#521)

--- python/tvm/meta_schedule/feature_extractor/__init__.py
+++ python/tvm/meta_schedule/feature_extractor/__init__.py
@@ -20,3 +20,4 @@ Meta Schedule feature extractors that extracts features from
 measure candidates for use in cost model.
 """
 from .feature_extractor import FeatureExtractor, PyFeatureExtractor
+from .per_store_feature import PerStoreFeature
