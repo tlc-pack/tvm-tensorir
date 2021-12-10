9a7d77584ff763bee7a8ee2995d8412e9a77e805
Junru Shao <junrushao1994@gmail.com>
2021-12-09 21:34:12 -0800

    fix fix fix

--- python/tvm/meta_schedule/cost_model/xgb_model.py
+++ python/tvm/meta_schedule/cost_model/xgb_model.py
@@ -388,7 +388,7 @@ class XGBModel(PyCostModel):
 
         def _mean_cost(x: RunnerResult) -> float:
             if not x.run_secs:
-                return 0.0
+                return 1e10
             return sum(float(s) for s in x.run_secs) / len(x.run_secs)
 
         new_features = [
