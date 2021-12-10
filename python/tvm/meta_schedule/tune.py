3be13883fe2ac3714ccd6c3cef85a991a9d67d3d
Xiyou Zhou <xiyou@octoml.ai>
2021-12-09 19:23:45 -0800

    Renmove unused module.

--- python/tvm/meta_schedule/tune.py
+++ python/tvm/meta_schedule/tune.py
@@ -600,30 +600,6 @@ def tune_te(
     )
 
 
-from tvm import script
-
-# pylint: disable=invalid-name,no-member,line-too-long,too-many-nested-blocks,missing-docstring
-
-
-from tvm.script import tir as T
-
-
-@script.ir_module
-class MatmulModule:
-    @T.prim_func
-    def matmul(a: T.handle, b: T.handle, c: T.handle) -> None:  # pylint: disable=no-self-argument
-        T.func_attr({"global_symbol": "matmul", "tir.noalias": True})
-        A = T.match_buffer(a, (1024, 1024), "float32")
-        B = T.match_buffer(b, (1024, 1024), "float32")
-        C = T.match_buffer(c, (1024, 1024), "float32")
-        for i, j, k in T.grid(1024, 1024, 1024):
-            with T.block("matmul"):
-                vi, vj, vk = T.axis.remap("SSR", [i, j, k])
-                with T.init():
-                    C[vi, vj] = 0.0
-                C[vi, vj] = C[vi, vj] + A[vi, vk] * B[vk, vj]
-
-
 def tune_relay(
     mod: Union[RelayFunc, IRModule],
     target: Union[str, Target],
