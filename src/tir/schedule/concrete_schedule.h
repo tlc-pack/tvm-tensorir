5af48418685b717300a15ccf4c70d84559f1a2a7
Ruihang Lai <lairuihangdongdong@qq.com>
2021-12-19 01:08:23 +0800

    Remove debug code

--- src/tir/schedule/concrete_schedule.h
+++ src/tir/schedule/concrete_schedule.h
@@ -262,7 +262,6 @@ inline StmtSRef ConcreteScheduleNode::GetSRef(const LoopRV& loop_rv) const {
                << (obj.defined() ? obj->GetTypeKey() : "None");
   }
   if (sref->stmt == nullptr) {
-    throw;
     LOG(FATAL) << "ValueError: The loop no longer exists in the IRModule";
   }
   return GetRef<StmtSRef>(sref);
