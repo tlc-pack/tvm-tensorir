35fbd679c3571f41b711bbc8f5f7cb4b521e745b
jinhongyi <3231950289@qq.com>
2021-12-19 11:56:35 +0800

    address comments

--- src/tir/transforms/memhammer_rewrite_rule.h
+++ src/tir/transforms/memhammer_rewrite_rule.h
@@ -28,22 +28,35 @@
 namespace tvm {
 namespace tir {
 
-/*! \brief the set containing all possible constraints */
+/*! \brief The set containing all possible constraints of a data copy*/
 struct ConstraintSet {
+  /*! \brief The extents of the thread binding loops*/
   Map<String, Integer> thread_extent;
+  /*! \brief The outer loops surrounding the data copy*/
   Array<For> outer_loops;
+  /*! \brief The read region of the data copy*/
   BufferRegion read_region;
+  /*! \brief The write region of the data copy*/
   BufferRegion write_region;
+  /*! \brief The dtype size in bits*/
   Integer data_bits;
+  /*! \brief Whether to insert a local stage in the data copy*/
   Integer add_local_stage = Integer(0);
+  /*! \brief The vectorization length in bytes*/
   Integer vector_bytes = 1;
 };
 
+/*! \brief The set containing all possible outpus of a rewrite rule*/
 struct OutputSet {
+  /*! \brief New buffers allocated after rewrite*/
   Array<Buffer> alloc_buffer;
+  /*! \brief The minimal padding size of a buffer in base 2 logarithm*/
   Map<Buffer, Integer> padding_min;
 };
 
+/*!
+ * \brief Rules to rewrite a data copy.
+ */
 class RewriteRule {
  private:
   /*!
@@ -74,6 +87,13 @@ class RewriteRule {
   }
 };
 
+inline bool IsCopyBetweenScope(const Buffer& src_buffer, const Buffer& tgt_buffer,
+                               runtime::StorageRank src_rank, runtime::StorageRank tgt_rank) {
+  runtime::StorageScope src_scope = runtime::StorageScope::Create(src_buffer.scope());
+  runtime::StorageScope tgt_scope = runtime::StorageScope::Create(tgt_buffer.scope());
+  return src_scope.rank == src_rank && tgt_scope.rank == tgt_rank;
+}
+
 /*!
  * \brief Coalesce and vectorize memory access.
  */
@@ -81,14 +101,12 @@ class CoalescedAccess : public RewriteRule {
  public:
   Stmt Rewrite(const Stmt& stmt, const ConstraintSet& constraints, OutputSet* output) const final;
   bool CanApply(const Stmt& stmt, const ConstraintSet& constraints) const final {
-    runtime::StorageScope src_scope =
-        runtime::StorageScope::Create(constraints.read_region->buffer.scope());
-    runtime::StorageScope tgt_scope =
-        runtime::StorageScope::Create(constraints.write_region->buffer.scope());
-    return (src_scope.rank == runtime::StorageRank::kGlobal &&
-            tgt_scope.rank == runtime::StorageRank::kShared) ||
-           (src_scope.rank == runtime::StorageRank::kShared &&
-            tgt_scope.rank == runtime::StorageRank::kGlobal);
+    Buffer src_buffer = constraints.read_region->buffer;
+    Buffer tgt_buffer = constraints.write_region->buffer;
+    return IsCopyBetweenScope(src_buffer, tgt_buffer, runtime::StorageRank::kGlobal,
+                              runtime::StorageRank::kShared) ||
+           IsCopyBetweenScope(src_buffer, tgt_buffer, runtime::StorageRank::kShared,
+                              runtime::StorageRank::kGlobal)
   }
 };
 
@@ -99,12 +117,10 @@ class InverseMapping : public RewriteRule {
  public:
   Stmt Rewrite(const Stmt& stmt, const ConstraintSet& constraints, OutputSet* output) const final;
   bool CanApply(const Stmt& stmt, const ConstraintSet& constraints) const final {
-    runtime::StorageScope src_scope =
-        runtime::StorageScope::Create(constraints.read_region->buffer.scope());
-    runtime::StorageScope tgt_scope =
-        runtime::StorageScope::Create(constraints.write_region->buffer.scope());
-    return src_scope.rank == runtime::StorageRank::kShared &&
-           tgt_scope.rank == runtime::StorageRank::kGlobal;
+    Buffer src_buffer = constraints.read_region->buffer;
+    Buffer tgt_buffer = constraints.write_region->buffer;
+    return IsCopyBetweenScope(src_buffer, tgt_buffer, runtime::StorageRank::kShared,
+                              runtime::StorageRank::kGlobal)
   }
 };
 
@@ -115,12 +131,11 @@ class CreateLocalStage : public RewriteRule {
  public:
   Stmt Rewrite(const Stmt& stmt, const ConstraintSet& constraints, OutputSet* output) const final;
   bool CanApply(const Stmt& stmt, const ConstraintSet& constraints) const final {
-    runtime::StorageScope src_scope =
-        runtime::StorageScope::Create(constraints.read_region->buffer.scope());
-    runtime::StorageScope tgt_scope =
-        runtime::StorageScope::Create(constraints.write_region->buffer.scope());
-    return src_scope.rank == runtime::StorageRank::kGlobal &&
-           tgt_scope.rank == runtime::StorageRank::kShared && is_one(constraints.add_local_stage);
+    Buffer src_buffer = constraints.read_region->buffer;
+    Buffer tgt_buffer = constraints.write_region->buffer;
+    return IsCopyBetweenScope(src_buffer, tgt_buffer, runtime::StorageRank::kGlobal,
+                              runtime::StorageRank::kShared) &&
+           is_one(constraints.add_local_stage);
   }
 };
 
@@ -132,12 +147,10 @@ class WmmaToGlobal : public RewriteRule {
  public:
   Stmt Rewrite(const Stmt& stmt, const ConstraintSet& constraints, OutputSet* output) const final;
   bool CanApply(const Stmt& stmt, const ConstraintSet& constraints) const final {
-    runtime::StorageScope src_scope =
-        runtime::StorageScope::Create(constraints.read_region->buffer.scope());
-    runtime::StorageScope tgt_scope =
-        runtime::StorageScope::Create(constraints.write_region->buffer.scope());
-    return src_scope.rank == runtime::StorageRank::kWMMAAccumulator &&
-           tgt_scope.rank == runtime::StorageRank::kGlobal;
+    Buffer src_buffer = constraints.read_region->buffer;
+    Buffer tgt_buffer = constraints.write_region->buffer;
+    return IsCopyBetweenScope(src_buffer, tgt_buffer, runtime::StorageRank::kWMMAAccumulator,
+                              runtime::StorageRank::kGlobal);
   }
 };
 
@@ -148,13 +161,12 @@ class SharedToWmma : public RewriteRule {
  public:
   Stmt Rewrite(const Stmt& stmt, const ConstraintSet& constraints, OutputSet* output) const final;
   bool CanApply(const Stmt& stmt, const ConstraintSet& constraints) const final {
-    runtime::StorageScope src_scope =
-        runtime::StorageScope::Create(constraints.read_region->buffer.scope());
-    runtime::StorageScope tgt_scope =
-        runtime::StorageScope::Create(constraints.write_region->buffer.scope());
-    return src_scope.rank == runtime::StorageRank::kShared &&
-           (tgt_scope.rank == runtime::StorageRank::kWMMAMatrixA ||
-            tgt_scope.rank == runtime::StorageRank::kWMMAMatrixB);
+    Buffer src_buffer = constraints.read_region->buffer;
+    Buffer tgt_buffer = constraints.write_region->buffer;
+    return IsCopyBetweenScope(src_buffer, tgt_buffer, runtime::StorageRank::kShared,
+                              runtime::StorageRank::kWMMAMatrixA) ||
+           IsCopyBetweenScope(src_buffer, tgt_buffer, runtime::StorageRank::kShared,
+                              runtime::StorageRank::kWMMAMatrixB);
   }
 };
 
@@ -165,12 +177,10 @@ class WmmaToShared : public RewriteRule {
  public:
   Stmt Rewrite(const Stmt& stmt, const ConstraintSet& constraints, OutputSet* output) const final;
   bool CanApply(const Stmt& stmt, const ConstraintSet& constraints) const final {
-    runtime::StorageScope src_scope =
-        runtime::StorageScope::Create(constraints.read_region->buffer.scope());
-    runtime::StorageScope tgt_scope =
-        runtime::StorageScope::Create(constraints.write_region->buffer.scope());
-    return src_scope.rank == runtime::StorageRank::kWMMAAccumulator &&
-           tgt_scope.rank == runtime::StorageRank::kShared;
+    Buffer src_buffer = constraints.read_region->buffer;
+    Buffer tgt_buffer = constraints.write_region->buffer;
+    return IsCopyBetweenScope(src_buffer, tgt_buffer, runtime::StorageRank::kWMMAAccumulator,
+                              runtime::StorageRank::kShared);
   }
 };
 
