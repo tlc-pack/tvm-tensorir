cb54c16ffd8c5171f460b281c32b96b0ab38a3a6
Ruihang Lai <lairuihangdongdong@qq.com>
2021-12-25 10:12:44 +0800

    Fix postproc RewriteUnboundBlock

--- src/meta_schedule/postproc/rewrite_unbound_block.cc
+++ src/meta_schedule/postproc/rewrite_unbound_block.cc
@@ -46,6 +46,7 @@ BindType GetBindType(const StmtSRef& block_sref, int* fuse_first_num) {
   int i_block_idx = -1;
   int i_thread_idx = -1;
   int i_multi_child = -1;
+  int i_spatial_loop = -1;
   for (int i = 0; i < n; ++i) {
     const StmtSRef& loop_sref = loops[i];
     const ForNode* loop = TVM_SREF_TO_FOR(loop, loop_sref);
@@ -65,6 +66,11 @@ BindType GetBindType(const StmtSRef& block_sref, int* fuse_first_num) {
         i_multi_child = i + 1;
       }
     }
+    if (tir::GetLoopIterType(loop_sref) == IterVarType::kDataPar) {
+      if (i_spatial_loop == i - 1) {
+        ++i_spatial_loop;
+      }
+    }
   }
   if (i_multi_child == -1) {
     i_multi_child = n;
@@ -75,10 +81,10 @@ BindType GetBindType(const StmtSRef& block_sref, int* fuse_first_num) {
     ICHECK(false) << "Unsupported case, where blockIdx is bound but threadIdx is not";
     throw;
   } else if (i_block_idx == -1 && i_thread_idx != -1) {
-    *fuse_first_num = std::min(i_multi_child, i_thread_idx);
+    *fuse_first_num = std::min(std::min(i_multi_child, i_thread_idx), i_spatial_loop + 1);
     return BindType::kBindBlock;
   } else {  // i_block_idx == -1 && i_thread_idx == -1
-    *fuse_first_num = i_multi_child;
+    *fuse_first_num = std::min(i_multi_child, i_spatial_loop + 1);
     return BindType::kBindBlockThread;
   }
 }
